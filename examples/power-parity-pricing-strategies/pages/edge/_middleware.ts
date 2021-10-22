50e4973215c26d920949eacc18fc772ffe51c4f6
Gonzalo Pozzo <gonzalo.pozzo4@gmail.com>
2021-10-22 11:54:07 -0300

    Add 404 page to layout (#124)
    * Add logs
    
    * Reset country

--- examples/power-parity-pricing-strategies/pages/edge/_middleware.ts
+++ examples/power-parity-pricing-strategies/pages/edge/_middleware.ts
@@ -2,11 +2,7 @@ import { NextFetchEvent, NextResponse } from 'next/server'
 
 export function middleware(ev: NextFetchEvent) {
   const req = ev.request
-  let country = req.geo.country?.toLowerCase() || 'us'
-
-  if (country.includes('-')) {
-    country = country.split('-')[1].toLowerCase();
-  }
+  const country = req.geo.country?.toLowerCase() || 'us'
 
   req.nextUrl.pathname = `/edge/${country}`
   ev.respondWith(NextResponse.rewrite(req.nextUrl))
