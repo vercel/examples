32349feb7df43a0b222304a5a418a4b034f29dd2
Sokratis Vidros <sokratis.vidros@gmail.com>
2021-10-26 18:45:46 +0300

    Add clerk-authentication example

---
+++ edge-functions/clerk-authentication/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          100: '#ebeffe',
+          500: '#476BF2',
+          600: '#335bf1',
+          700: '#2e52d9',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
