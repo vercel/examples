600f38ca6e888af8cd8a6854d119f5a5e10ddd4d
Aziz Abbas <aziz@builder.io>
2021-10-26 09:46:05 -0700

    update to the new api

--- edge-functions/personalization-builder-io/pages/_middleware.tsx
+++ edge-functions/personalization-builder-io/pages/_middleware.tsx
@@ -1,19 +1,19 @@
-import { NextFetchEvent, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { getPersonalizedRewrite } from '@builder.io/personalization-utils'
 
 const excludededPrefixes = ['/favicon', '/api']
 
-export default function middleware(event: NextFetchEvent) {
-  const url = event.request.nextUrl
+export default function middleware(request: NextRequest) {
+  const url = request.nextUrl
   let response = NextResponse.next()
   if (!excludededPrefixes.find((path) => url.pathname?.startsWith(path))) {
     const rewrite = getPersonalizedRewrite(
       url?.pathname!,
-      event.request.cookies
+      request.cookies
     )
     if (rewrite) {
       response = NextResponse.rewrite(rewrite)
     }
   }
-  event.respondWith(response)
+  return response
 }
