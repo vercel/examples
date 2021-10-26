156d8c5fd3396858a2a2f09fa2bdc703d0cd050c
Luis Alvarez <luis@vercel.com>
2021-10-26 02:29:41 -0500

    Added a descriptive comment of datadome behavior

--- edge-functions/bot-protection-datadome/pages/_middleware.tsx
+++ edge-functions/bot-protection-datadome/pages/_middleware.tsx
@@ -3,7 +3,18 @@ import { first } from '@lib/utils'
 import datadome from '@lib/datadome'
 import demoMiddleware from '@lib/demo-middleware'
 
-function handler(req: NextRequest) {
+async function handler(req: NextRequest) {
+  // `datadome(req)` returns a promise that resolves to
+  // a respones (NextResponse) or undefined
+  //
+  // If there's a response, we made a Datadome request.
+  //
+  // If the response has a body, it means the request
+  // was blocked and we should return it, this would
+  // be the captcha page of Datadome
+  //
+  // If there's no body, you're not a bot and we
+  // send the response to include Datadome's headers.
   return datadome(req)
 }
 
