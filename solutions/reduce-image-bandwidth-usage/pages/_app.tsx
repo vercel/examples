394d0e086af2d9ac7f681e382192b50cfc7619ca
goncy <gonzalo.pozzo4@gmail.com>
2021-11-11 17:49:30 -0300

    Add reduce image bandwidth example

---
+++ solutions/reduce-image-bandwidth-usage/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app'
+
+import '../styles/globals.css'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App
