f71f4cc9bba51161508b4c9b107a982a6fa44b9e
LFades <luis@vercel.com>
2021-12-10 21:02:32 -0600

    Update to canary and move global css imports

--- solutions/monorepo/app/next.config.js
+++ solutions/monorepo/app/next.config.js
@@ -1,7 +1,6 @@
 const ntm = require('next-transpile-modules')
 
-module.exports = ntm([
-  '@vercel/edge-functions-ui',
-  '@company/ui',
-  '@company/utils',
-])
+module.exports = ntm(
+  ['@vercel/edge-functions-ui', '@company/ui', '@company/utils'],
+  { resolveSymlinks: false }
+)
--- solutions/monorepo/app/pages/_app.tsx
+++ solutions/monorepo/app/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import type { LayoutProps } from '@vercel/edge-functions-ui/layout'
 import { getLayout } from '@vercel/edge-functions-ui'
-import '@vercel/edge-functions-ui/globals.css'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
