6c1482d453eb2e3f6dd9ba66b5467b0ca001dcd3
Dom Sip <dom@vercel.com>
2022-02-02 12:43:48 +0000

    plop basic example

---
+++ plop-templates/example/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [require('@vercel/examples-ui/tailwind')],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    'node_modules/@vercel/examples-ui/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {},
+  },
+}
--- plopfile.js
+++ plopfile.js
@@ -1,5 +1,5 @@
 module.exports = function (plop) {
-  function getFileName(str) {
+  const transformName = (str) => {
     return str.toLowerCase().replace(/ /g, '-')
   }
 
@@ -21,14 +21,98 @@ module.exports = function (plop) {
           { name: 'Solutions', value: 'solutions' },
         ],
       },
+      {
+        type: 'checkbox',
+        name: 'options',
+        message: 'What options do you like?',
+        choices: [
+          { name: 'Next.js Middleware Function', value: 'middleware' },
+          { name: 'Tailwind CSS', value: 'tailwind', checked: true },
+          {
+            name: 'Vercel Serverless Functions: Hello world',
+            value: 'vercel-api',
+          },
+          {
+            name: 'Next.js API Routes - Serverless Functions: Hello world',
+            value: 'next-api-pages',
+            checked: true,
+          },
+          {
+            name: 'Vercel.json file',
+            value: 'vercel.json',
+          },
+        ],
+      },
     ],
-    actions: function (data) {
-      const exampleFolderName = getFileName(data.name)
+    actions: (data) => {
+      const plopExampleName = transformName(data.name)
+      const plopPath = `${data.exampleScopeFolder}/${plopExampleName}`
+
+      const filesToAlwaysCopyOver = [
+        'README.md',
+        'tsconfig.json',
+        '.eslintrc.json',
+        'next.config.js',
+        '.gitignore',
+        'next-env.d.ts',
+        'package.json',
+        'pages/index.tsx',
+        'pages/_app.tsx',
+      ]
+
       return [
+        ...filesToAlwaysCopyOver.map((file) => {
+          return {
+            type: 'add',
+            path: `{{exampleScopeFolder}}/${plopExampleName}/${file}`,
+            templateFile: `plop-templates/example/${file}`,
+          }
+        }),
+        // README.md
         {
-          type: 'add',
-          path: '{{data.exampleScopeFolder}}/{{exampleFolderName}}/README.md',
-          templateFile: 'plop-templates/example/README.md',
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/README.md`,
+          pattern: /(-- PLOP TITLE HERE --)/gi,
+          template: `${data.name}`,
+        },
+        {
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/README.md`,
+          pattern: /(-- PLOP EXAMPLE NAME HERE --)/gi,
+          template: `${plopExampleName}`,
+        },
+        {
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/README.md`,
+          pattern: /(-- PLOP PATH HERE --)/gi,
+          template: `${plopPath}`,
+        },
+        // package.json
+        {
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/package.json`,
+          pattern: /(-- PLOP EXAMPLE NAME HERE --)/gi,
+          template: `${plopExampleName}`,
+        },
+        // _app.tsx
+        {
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/_app.tsx`,
+          pattern: /(-- PLOP TITLE HERE --)/gi,
+          template: `${data.name}`,
+        },
+        {
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/_app.tsx`,
+          pattern: /(-- PLOP PATH HERE --)/gi,
+          template: `${plopPath}`,
+        },
+        // pages/index.tsx
+        {
+          type: 'modify',
+          path: `{{exampleScopeFolder}}/${plopExampleName}/pages/index.tsx`,
+          pattern: /(-- PLOP TITLE HERE --)/gi,
+          template: `${data.name}`,
         },
       ]
     },
