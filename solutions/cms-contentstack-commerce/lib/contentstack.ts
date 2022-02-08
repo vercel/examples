6377609bbdc721c894adae670887b996125efd59
Bel Curcio <curciobel@gmail.com>
2022-02-08 09:48:40 +0100

    Adding Delivery Auth

--- solutions/cms-contentstack-commerce/lib/contentstack.ts
+++ solutions/cms-contentstack-commerce/lib/contentstack.ts
@@ -1,11 +1,14 @@
 import createFetch from '@vercel/fetch'
 const fetch = createFetch()
 
-const API_URL = `https://api.contentstack.io/v3`
+const API_URL = `https://cdn.contentstack.io/v3`
 
-if (!process.env.CONTENTSTACK_API_KEY || !process.env.CONTENTSTACK_MGMT_TOKEN) {
+if (
+  !process.env.CONTENTSTACK_API_KEY ||
+  !process.env.CONTENTSTACK_ACCESS_TOKEN
+) {
   throw new Error(
-    'CONTENTSTACK_API_KEY or CONTENTSTACK_MGMT_TOKEN Environment variables are not correctly set. '
+    'CONTENTSTACK_API_KEY or CONTENTSTACK_ACCESS_TOKEN Environment variables are not correctly set. '
   )
 }
 
@@ -15,17 +18,21 @@ export default {
     entryId = '',
     locale = 'en-US'
   ): Promise<Entry | null> {
+    // https://www.contentstack.com/docs/developers/apis/content-delivery-api/#get-a-single-entry
     try {
       const res: any = await fetch(
-        `${API_URL}/content_types/${contentType}/entries/${entryId}?locale=${locale.toLocaleLowerCase()}`,
+        `${API_URL}/content_types/${contentType}/entries/${entryId}?locale=${locale.toLocaleLowerCase()}&environment=${
+          process.env.NODE_ENV
+        }`,
         {
           method: 'GET',
           headers: {
             api_key: process.env.CONTENTSTACK_API_KEY as string,
-            authorization: process.env.CONTENTSTACK_MGMT_TOKEN as string,
+            access_token: process.env.CONTENTSTACK_ACCESS_TOKEN as string,
           },
         }
       )
+
       const { entry } = await res.json()
       return entry
     } catch (err) {
