f7ca47b6120a4d5bda3bb41eef9deaac6ae8664e
goncy <gonzalo.pozzo4@gmail.com>
2022-02-14 11:17:50 -0300

    Add modify headers example

---
+++ edge-functions/modify-headers/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import type { LayoutProps } from '@vercel/examples-ui/layout'
+
+import { getLayout } from '@vercel/examples-ui'
+
+import '@vercel/examples-ui/globals.css'
+
+function App({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component)
+
+  return (
+    <Layout
+      title="Modify headers"
+      path="edge-functions/modify-headers"
+    >
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default App
---
+++ edge-functions/modify-headers/pages/index.tsx
@@ -0,0 +1,72 @@
+import type { FC } from 'react'
+
+import Head from 'next/head'
+import { Code, Layout, Page, Text } from '@vercel/examples-ui'
+
+const Snippet: FC = ({ children }) => {
+  return (
+    <pre className="border-accents-2 border rounded-md bg-white overflow-x-auto p-6 transition-all">
+      {children}
+    </pre>
+  )
+}
+
+const Console: FC = ({ children }) => {
+  return (
+    <pre className="bg-black text-white font-mono text-left py-2 px-4 rounded-lg text-sm leading-6">
+      {children}
+    </pre>
+  )
+}
+
+function IndexPage() {
+  return (
+    <Page>
+      <Head>
+        <title>Modifying headers at the edge</title>
+        <meta
+          name="description"
+          content="How to modify headers from an incoming request"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="flex flex-col gap-6">
+        <Text variant="h1">Modifying headers at the edge</Text>
+        <Text>
+          In some cases we might want to modify the headers from an incoming request for different reasons, AB testing, personalization, analytics, etc. To have access to the tenative response whe can call <Code>NextResponse.next()</Code> and then modify its headers:
+        </Text>
+        <Snippet>
+          {`// Store the response so we can modify its headers
+const response = NextResponse.next()
+
+// Set custom header
+response.headers.set(
+  "x-modified-edge",
+  "true"
+)
+
+// Return response
+return response`}
+        </Snippet>
+      </section>
+
+      <hr className="border-t border-accents-2 my-6" />
+
+      <section className="flex flex-col items-center gap-3">
+        <Text>
+          Open the network tab in devtools to see the response headers
+        </Text>
+        <Console>
+          <p>
+            <strong>{'x-modified-edge: '}</strong>
+            true
+          </p>
+        </Console>
+      </section>
+    </Page>
+  )
+}
+
+IndexPage.Layout = Layout
+
+export default IndexPage
