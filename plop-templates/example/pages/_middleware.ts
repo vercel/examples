73485b32d62daf7e3ba7a31f8ea600bef758e2f3
LFades <luis@vercel.com>
2022-02-14 17:01:30 -0500

    Updated _middleware

--- plop-templates/example/pages/_middleware.ts
+++ plop-templates/example/pages/_middleware.ts
@@ -1,5 +1,10 @@
 import type { NextFetchEvent, NextRequest } from 'next/server'
 
+const PUBLIC_FILE = /\.(.*)$/
+
 export default function middleware(req: NextRequest, event: NextFetchEvent) {
-  return new Response('Hello, world!')
+  // Only rewrite files that don't have a file extension
+  if (!PUBLIC_FILE.test(req.nextUrl.pathname)) {
+    return new Response('Hello world from pages/_middleware.ts')
+  }
 }
