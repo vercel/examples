cc4ca31e9fc965c51a62743d69194cbcea8a8049
thehouseavax <98769585+thehouseavax@users.noreply.github.com>
2022-02-17 10:54:32 -0500

    update docs

--- solutions/web3-fetching-strategies/pages/index.tsx
+++ solutions/web3-fetching-strategies/pages/index.tsx
@@ -3,6 +3,27 @@ import { Layout, Text, Page, Link } from '@vercel/examples-ui'
 import { BAYCAbi__factory } from '../types/ethers-contracts'
 import { ethers } from 'ethers'
 import useSWR from 'swr'
+import { FC } from 'react'
+
+// import abi from '../lib/BAYC.abi.json'
+
+// const contractAddress = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
+
+// const contract = new ethers.Contract(contractAddress, abi)
+
+/**
+ *
+ * @param param0
+ * @returns
+ */
+
+const Snippet: FC = ({ children }) => {
+  return (
+    <pre className="border-accents-2 border rounded-md bg-white overflow-x-auto p-6 transition-all">
+      {children}
+    </pre>
+  )
+}
 
 const contract = BAYCAbi__factory.connect(
   '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D',
@@ -24,51 +45,70 @@ function Home({ contractName }: { contractName: string }) {
       </Head>
 
       <section className="flex flex-col gap-6">
-        <Text variant="h1">web3-fetching-strategies usage example</Text>
+        <Text variant="h1">Fetching data from smart contracts</Text>
         <Text>
-          This example shows diffetent ways of fetching data from the
-          blockchain. Smart contracts often expose{' '}
-          <Link
-            className="underline"
-            href="https://www.tutorialspoint.com/solidity/solidity_view_functions.htm"
-          >
-            View functions
+          Smart contracts contain relevant information to applications built on
+          top of blockchains that can run the{' '}
+          <Link href="https://ethereum.org/en/developers/docs/evm/">
+            Ethereum Virtual Machine
           </Link>
-          . These functions can be queries without any transaction costs and are
-          a convenient way to get current information from a smart contract.
-        </Text>
-        <Text>
-          Unless they reqire a user to be authenticaed trough his wallet, these
-          functions can be called both from the client and the server.
-        </Text>
-      </section>
-
-      <hr className="border-t border-accents-2 my-6" />
-
-      <section className="flex flex-col gap-6">
-        <Text variant="h2">
-          The name {contractName} was fetched serverside. The same name:
-          {data} is also fetcheable via SWR in the client.
-        </Text>
-
-        <Text>
-          Achieving so was done by using the library{' '}
-          <Link className="underline" href="https://docs.ethers.io/v5/">
-            Ethers.js
+          . Some of these contracts informations can be exposed in the form of
+          View functions that do not need{' '}
+          <Link href="https://ethereum.org/en/developers/docs/gas/">
+            {' '}
+            gas or fees
           </Link>{' '}
-          and passing the informatin of a Smart Contract such as its ABI and the
-          smart contract Address. The same could be achieved client side. It
-          would be suggested to use the SWR library to validate changed
-          information as smart contract view functions normally indicate the
-          current state of a smart contract.
+          to be executed. Let's explore how to get that information in Next.js
+        </Text>
+
+        <Text variant="h2">
+          Instanciating a connection to the smart contract
         </Text>
         <Text>
-          The same strategy of using SWR should be used when dealing with
-          information that required the user to be authenticated using a wallet
-          such as Metamask as this Data becomes only available Client Side.
-          Passing your ethers.js contract instance as the fetcher to SWR works
-          well.
+          A first step needed to contact these smart contract via RPC is to
+          instanciate a connection to them using a library like{' '}
+          <Link href="https://docs.ethers.io/v5/">Ethers.js</Link>.
         </Text>
+        <Text>
+          The ABI contains information about the available function. Get it from
+          Etherscan like explained{' '}
+          <Link href="https://cryptomarketpool.com/how-to-get-a-smart-contracts-abi-for-use-in-python-web3-py/">
+            Here
+          </Link>
+          . We will use the Bored Ape Yatch Club popular NFT contract.
+        </Text>
+        <Snippet>
+          {`
+import abi from '../lib/BAYC.abi.json'
+
+const contractAddress = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
+
+const contract = new ethers.Contract(contractAddress, abi)
+`}
+        </Snippet>
+        <Text variant="h2">Fetching the data</Text>
+
+        <Text>
+          This can now be used in getServerSideProps or getStaticProps and even
+          SWR if you are dealing with a smart contract that is quite active and
+          has a lot of usage.
+        </Text>
+        <Snippet>
+          {`// Server side
+export async function getServerSideProps() {
+  const contractName = await contract.name()
+  return {
+    props: {
+      contractName,
+    },
+  }
+}
+
+// with SWR
+const { data } = useSWR('name', () => contract.name())
+`}
+        </Snippet>
+        <Text>It's not more complicated than this!</Text>
       </section>
     </Page>
   )
