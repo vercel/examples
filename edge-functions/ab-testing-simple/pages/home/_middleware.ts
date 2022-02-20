f80c25375c1adbd8da4e247d727019630aa3d23a
lidqqq <lidqqq.dev@gmail.com>
2022-02-20 11:58:24 +0900

    :bug: fix: Edge Functions cause 500 Error with `Next.js@12.1`

--- edge-functions/ab-testing-simple/pages/home/_middleware.ts
+++ edge-functions/ab-testing-simple/pages/home/_middleware.ts
@@ -7,7 +7,9 @@ const COOKIE_NAME = 'bucket-home'
 export function middleware(req: NextRequest) {
   // Get the bucket cookie
   const bucket = req.cookies[COOKIE_NAME] || getBucket(HOME_BUCKETS)
-  const res = NextResponse.rewrite(`/home/${bucket}`)
+  const destination = req.nextUrl.clone()
+  destination.pathname = `/home/${bucket}`
+  const res = NextResponse.rewrite(destination)
 
   // Add the bucket to cookies if it's not there
   if (!req.cookies[COOKIE_NAME]) {
--- edge-functions/ab-testing-simple/pages/marketing/_middleware.ts
+++ edge-functions/ab-testing-simple/pages/marketing/_middleware.ts
@@ -7,7 +7,9 @@ const COOKIE_NAME = 'bucket-marketing'
 export function middleware(req: NextRequest) {
   // Get the bucket cookie
   const bucket = req.cookies[COOKIE_NAME] || getBucket(MARKETING_BUCKETS)
-  const res = NextResponse.rewrite(`/marketing/${bucket}`)
+  const destination = req.nextUrl.clone()
+  destination.pathname = `/marketing/${bucket}`
+  const res = NextResponse.rewrite(destination)
 
   // Add the bucket to cookies if it's not there
   if (!req.cookies[COOKIE_NAME]) {
