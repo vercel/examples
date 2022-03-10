3edb965e96436f088c65f107f7891767c73bd456
LFades <luis@vercel.com>
2022-03-09 22:08:03 -0500

    Removed unrequired useEffect

--- edge-functions/serverside-analytics/pages/_app.tsx
+++ edge-functions/serverside-analytics/pages/_app.tsx
@@ -1,15 +1,11 @@
 import type { AppProps } from 'next/app'
 import type { LayoutProps } from '@vercel/examples-ui/layout'
-
 import { getLayout } from '@vercel/examples-ui'
-
 import '@vercel/examples-ui/globals.css'
-import { useEffect } from 'react'
 
 function App({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
 
-  useEffect(() => {})
   return (
     <Layout
       title="Serverside Analytics"
