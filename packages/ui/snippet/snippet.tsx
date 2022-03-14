1f5e2fd4d2c763670c204a096860a168794eff3e
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-03-14 13:24:17 -0400

    suggar high snippet added

---
+++ packages/ui/snippet/snippet.tsx
@@ -0,0 +1,13 @@
+import s from './snippet.module.css'
+import { highlight } from 'sugar-high'
+
+const Snippet: React.FC = ({ children }) => {
+  const __html = children ? highlight(children?.toString()!) : ''
+  return (
+    <pre className={s.root}>
+      <code dangerouslySetInnerHTML={{ __html }} />
+    </pre>
+  )
+}
+
+export default Snippet
