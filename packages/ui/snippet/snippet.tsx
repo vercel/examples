cb6aaef689966e680eadf5131af18f742014cda9
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-03-14 15:21:44 -0400

    theme from vercel and lock version

--- packages/ui/snippet/snippet.tsx
+++ packages/ui/snippet/snippet.tsx
@@ -1,8 +1,12 @@
+import React from 'react'
 import s from './snippet.module.css'
 import { highlight } from 'sugar-high'
 
 const Snippet: React.FC = ({ children }) => {
-  const __html = children ? highlight(children?.toString()!) : ''
+  const __html = React.useMemo(
+    () => (children ? highlight(children.toString()!) : ''),
+    [children]
+  )
   return (
     <pre className={s.root}>
       <code dangerouslySetInnerHTML={{ __html }} />
