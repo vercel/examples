58a2c093a330f4523a30052396c9f018bd144e4b
Tobias Speicher <rootcommander@gmail.com>
2022-03-18 02:59:06 +0100

    chore: replace deprecated String.prototype.substr()
    .substr() is deprecated so we replace it with .slice() which works similarily but isn't deprecated
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- solutions/monorepo/packages/utils/src/index.ts
+++ solutions/monorepo/packages/utils/src/index.ts
@@ -13,9 +13,9 @@ export function randomColor() {
  * our demo purposes.
  */
 export function matchingTextColor(color: string) {
-  const r = parseInt(color.substr(1, 2), 16)
-  const g = parseInt(color.substr(3, 2), 16)
-  const b = parseInt(color.substr(5, 2), 16)
+  const r = parseInt(color.slice(1, 3), 16)
+  const g = parseInt(color.slice(3, 5), 16)
+  const b = parseInt(color.slice(5, 7), 16)
   const yiq = (r * 299 + g * 587 + b * 114) / 1000
 
   return yiq >= 128 ? '#000' : '#fff'
