1efb5f71e3cd199c66c5a22255c12ba975256984
Zeeshan Ashraf <35262377+zashraf1985@users.noreply.github.com>
2022-03-29 14:53:40 -0700

    Optimizely Feature flag example (#144)
    * Optimizely Feature flag integration with Vercel Edge functions (#2)
    
    * Update Readme Description (#3)
    
    * 1. Updated Optimizely SDK version to latest
    2. Updated NextJS version to latest
    3. Removed yarn.lock and generated package-lock.json to be consistent with vercel.
    
    * Added More details to readme and application pages (#4)
    
    * fixed an error
    
    Co-authored-by: Danny-Driscoll <daniel.driscoll@optimizely.com>

---
+++ edge-functions/feature-flag-optimizely/pages/_app.tsx
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import type { LayoutProps } from '@vercel/edge-functions-ui/layout'
+import { getLayout } from '@vercel/edge-functions-ui'
+import '@vercel/edge-functions-ui/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component)
+  
+  return (
+    <Layout
+      title="Feature Flagging with Optimizely"
+      path="feature-flag-optimizely"
+      deployButton={{
+        env: ['OPTIMIZELY_SDK_KEY'],
+        repositoryUrl: 'https://github.com/optimizely/vercel-examples/tree/main/edge-functions/feature-flag-optimizely'
+      }}
+    >
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
