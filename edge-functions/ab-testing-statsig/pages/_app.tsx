b2729469ec8da04aa2acd4e4cf16bef5f50e5b6e
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-04-05 12:47:00 -0400

    add metrics

--- edge-functions/ab-testing-statsig/pages/_app.tsx
+++ edge-functions/ab-testing-statsig/pages/_app.tsx
@@ -1,17 +1,31 @@
 import type { AppProps } from 'next/app'
 import type { LayoutProps } from '@vercel/examples-ui/layout'
+import { StatsigProvider } from 'statsig-react'
 
 import { getLayout } from '@vercel/examples-ui'
+import Cookies from 'js-cookie'
 
 import '@vercel/examples-ui/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
 
+  // middleware will automatically set a cookie for the user if they visit a page
+  const userID = Cookies.get('uid')
+
   return (
-    <Layout title="ab-testing-statsig" path="edge-functions/ab-testing-statsig">
-      <Component {...pageProps} />
-    </Layout>
+    <StatsigProvider
+      sdkKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
+      waitForInitialization={false}
+      user={{ userID }}
+    >
+      <Layout
+        title="ab-testing-statsig"
+        path="edge-functions/ab-testing-statsig"
+      >
+        <Component {...pageProps} />
+      </Layout>
+    </StatsigProvider>
   )
 }
 
--- edge-functions/ab-testing-statsig/pages/_bucket/[bucket].tsx
+++ edge-functions/ab-testing-statsig/pages/_bucket/[bucket].tsx
@@ -11,30 +11,31 @@ import {
 import { useRouter } from 'next/router'
 import Cookie from 'js-cookie'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import { Statsig } from 'statsig-react'
 
 interface Props {
-  bucket: string;
+  bucket: string
 }
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   return {
     props: {
-      bucket: params?.bucket as string
-    }
+      bucket: params?.bucket as string,
+    },
   }
 }
 
 export const getStaticPaths: GetStaticPaths<{ bucket: string }> = async () => {
   // Buckets that we want to statically generate
-  const buckets: string[] = ['a', 'b'];
+  const buckets: string[] = ['a', 'b']
 
   return {
     paths: buckets.map((bucket) => ({
       params: {
-        bucket
-      }
+        bucket,
+      },
     })),
-    fallback: 'blocking'
+    fallback: 'blocking',
   }
 }
 
@@ -43,7 +44,7 @@ function BucketPage({ bucket }: Props) {
 
   function resetBucket() {
     Cookie.remove('uid')
-
+    Statsig.logEvent('reset-bucket')
     reload()
   }
 
@@ -127,7 +128,7 @@ export async function middleware(req: NextRequest) {
         <Text>
           You can reset the bucket multiple times to get a different bucket
           assigned. You can configure your experiments, see diagnostics and
-          results in your account{' '}
+          results in your account .
           <Link href="https://console.statsig.com/">Statsig console</Link>.
         </Text>
         <pre className="bg-black text-white font-mono text-left py-2 px-4 rounded-lg text-sm leading-6">
@@ -136,6 +137,45 @@ export async function middleware(req: NextRequest) {
         <Button size="lg" onClick={resetBucket}>
           Reset bucket
         </Button>
+
+        <Text variant="h2">Using metrics in your experiments</Text>
+        <Text>
+          <Link href="https://docs.statsig.com/metrics">Statsig Metrics</Link>{' '}
+          are a way to track events that happen in your site. To enable them you
+          can can pass to{' '}
+          <Link href="https://nextjs.org/docs/advanced-features/custom-app">
+            <Code>_app.tsx</Code>
+          </Link>{' '}
+          the <Code>StatsigProvider</Code>.
+        </Text>
+        <Snippet>{`import Cookies from 'js-cookie'
+
+function App({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component)
+  // middleware will automatically set a cookie for the user if they visit a page
+  const userID = Cookies.get('uid')
+  return (
+    <StatsigProvider
+      sdkKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
+      waitForInitialization={true}
+      user={{ userID }}
+    >
+      <Layout title="statsig-metric" path="solutions/statsig-metric">
+        <Component {...pageProps} />
+      </Layout>
+    </StatsigProvider>
+  )
+}
+`}</Snippet>
+        <Text>
+          After we can tracks events by calling using the{' '}
+          <Code>Statsig.logEvent</Code> function to track events during your
+          experiments.
+        </Text>
+        <Snippet>{`import { Statsig } from 'statsig-react'
+
+Statsig.logEvent('my-event')
+`}</Snippet>
       </section>
     </Page>
   )
