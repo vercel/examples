e4b5d3bf0640887b9aecbd5edc66898b5315dcf4
Lee Robinson <me@leerob.io>
2022-04-06 12:08:03 -0500

    Update am-i-blocked.tsx

--- edge-functions/api-rate-limit-and-tokens/pages/am-i-blocked.tsx
+++ edge-functions/api-rate-limit-and-tokens/pages/am-i-blocked.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Layout, Text } from '@vercel/examples-ui'
 
 const Index = () => (
-  <div className="w-full max-w-3xl mx-auto pt-16 mb-4">
+  <div className="w-full max-w-3xl mx-auto mb-4">
     <div className="text-center mb-6">
       <Text variant="h1" className="mb-4">
         IP Blocking with Upstash
