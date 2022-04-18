99dcdb37cc102c7b175cad5f7b464a4f79e32538
Guillaume Bibeau-laviolette <g.bibeaulaviolette@gmail.com>
2022-04-18 14:13:58 -0400

    Update web3/sessions to become web3/auth (#220)
    * update to be web3 auth instead
    
    * delete hello world function
    
    * url solutions prefix
    
    * don't delete session

---
+++ solutions/web3-authentication/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from 'next/app'
+import { WagmiProvider } from 'wagmi'
+import { SessionProvider } from 'next-auth/react'
+
+import type { LayoutProps } from '@vercel/examples-ui/layout'
+
+import { getLayout } from '@vercel/examples-ui'
+
+import '@vercel/examples-ui/globals.css'
+
+function App({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component)
+
+  return (
+    <WagmiProvider autoConnect>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Layout
+          title="web3-authentication"
+          path="solutions/web3-authentication"
+        >
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </WagmiProvider>
+  )
+}
+
+export default App
