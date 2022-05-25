c428703a035163fa3397ffed370f689871a6e2eb
LFades <luis@vercel.com>
2022-05-25 16:15:59 -0500

    Updated tailwind config in packages

--- solutions/microfrontends/packages/design-system/src/tailwind.cjs
+++ solutions/microfrontends/packages/design-system/src/tailwind.cjs
@@ -1,14 +1,4 @@
-const path = require('path')
-
 module.exports = {
-  // `content` is replaced instead of extended, so this line has to be added in
-  // the `content` of each app' tailwind.config.js
-  content: [
-    path.join(
-      path.dirname(require.resolve('@acme/design-system')),
-      '**/*.{js,ts,jsx,tsx}'
-    ),
-  ],
   theme: {
     extend: {
       colors: {
