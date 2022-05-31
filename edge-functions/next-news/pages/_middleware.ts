112cce6a335d891c643e9562ee67d663b62eac95
Guillaume Bibeau-laviolette <g.bibeaulaviolette@gmail.com>
2022-05-30 20:37:52 -0400

    remove middleware for something that can be done with a regular rewrite (#267)

--- edge-functions/next-news/pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-
-export default function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone()
-  if (url.pathname === '/') {
-    url.pathname = '/news/1'
-    return NextResponse.rewrite(url)
-  }
-}
