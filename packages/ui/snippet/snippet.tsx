a5049788a1e4f0da88c5e4edce2724776db4c7d0
LFades <luis@vercel.com>
2022-05-31 16:47:31 -0500

    Updated snippet component

--- packages/ui/snippet/snippet.tsx
+++ packages/ui/snippet/snippet.tsx
@@ -2,13 +2,16 @@ import { FC, HTMLAttributes, useMemo } from 'react'
 import s from './snippet.module.css'
 import { highlight } from 'sugar-high'
 
-const Snippet: FC<HTMLAttributes<HTMLPreElement>> = ({ children }) => {
+const Snippet: FC<HTMLAttributes<HTMLPreElement>> = ({
+  children,
+  ...props
+}) => {
   const __html = useMemo(
     () => (children ? highlight(children.toString()!) : ''),
     [children]
   )
   return (
-    <pre className={s.root}>
+    <pre className={s.root} {...props}>
       <code dangerouslySetInnerHTML={{ __html }} />
     </pre>
   )
