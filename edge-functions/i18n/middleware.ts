4dbdd9dfd460fa48250122c1072cde6e1c29f3dd
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-02 15:46:34 -0400

    trying something

--- edge-functions/i18n/middleware.ts
+++ edge-functions/i18n/middleware.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from 'next/server'
 const PUBLIC_FILE = /\.(.*)$/
 
 export function middleware(req: NextRequest) {
-  console.log('running middleware!!!!!!!')
   const country = req.geo.country?.toLowerCase() || 'us'
   const locale = req.headers.get('accept-language')?.split(',')?.[0] || 'en-US'
 
