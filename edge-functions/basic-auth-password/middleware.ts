c7b44ec92713ffebb45a4cba23b22c00f370904f
Dom Sip <dom@vercel.com>
2022-06-06 14:15:11 +0100

    update basic auth middleware

--- edge-functions/basic-auth-password/middleware.ts
+++ edge-functions/basic-auth-password/middleware.ts
@@ -1,21 +1,23 @@
+/* eslint-disable @next/next/no-server-import-in-page */
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname !== '/') {
+    return
+  }
+
   const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl.clone()
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(auth).split(":")
+    const [user, pwd] = atob(auth).split(':')
 
     if (user === '4dmin' && pwd === 'testpwd123') {
       return NextResponse.next()
     }
   }
+  url.pathname = `/auth`
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  })
+  return NextResponse.rewrite(url)
 }
