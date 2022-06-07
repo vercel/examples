5ad40ffbae752a6c83ecff682873811c94e5488e
goncy <gonzalo.pozzo4@gmail.com>
2022-06-07 16:50:08 -0300

    update to use matching

--- edge-functions/power-parity-pricing/middleware.ts
+++ edge-functions/power-parity-pricing/middleware.ts
@@ -1,19 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// Set a path matcher
-const PATH_MATCHER = new RegExp(/\/[a-z]{2}$/g)
-
 // Set pathname were middleware will be executed
 export const config = {
-  pathname: '/',
+  matcher: '/',
 }
 
 export function middleware(req: NextRequest) {
   // Clone URL
   const url = req.nextUrl.clone()
-
-  // Skip unwanted paths
-  if (PATH_MATCHER.test(url.pathname)) return NextResponse.next()
 
   // Get country
   const country = req.geo.country?.toLowerCase() || 'us'
