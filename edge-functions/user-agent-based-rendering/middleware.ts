6f16a52f162f5af5008dad70cf64e73f34b3b7ae
goncy <gonzalo.pozzo4@gmail.com>
2022-06-07 16:30:52 -0300

    update to use matching

--- edge-functions/user-agent-based-rendering/middleware.ts
+++ edge-functions/user-agent-based-rendering/middleware.ts
@@ -1,23 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
 import parser from 'ua-parser-js'
 
-// RegExp for public files
-const PUBLIC_FILE = /\.(.*)$/
-
-// RegExp for public files
-const NEXT_FILES = /^\/_next(.*?)/
-
 // Set pathname were middleware will be executed
 export const config = {
-  pathname: '/',
+  matcher: '/',
 }
 
 export function middleware(req: NextRequest) {
   // Clone the URL
   const url = req.nextUrl.clone()
-
-  // Skip public files
-  if (PUBLIC_FILE.test(url.pathname) || NEXT_FILES.test(url.pathname)) return
 
   // Parse user agent
   const ua = parser(req.headers.get('user-agent')!)
