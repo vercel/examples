db7a3dd0aefdb2ee5d62d89a6624768313590d45
goncy <gonzalo.pozzo4@gmail.com>
2022-06-07 16:44:38 -0300

    update to use matching

--- edge-functions/rewrites-upstash/middleware.ts
+++ edge-functions/rewrites-upstash/middleware.ts
@@ -1,31 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
 import api from './api'
 
-// RegExp for public files
-const PUBLIC_FILE = /\.(.*)$/
-
-// RegExp for public files
-const NEXT_FILES = /^\/_next\//
-
-// RegExp for match path
-const MATCHED_PATHS = /^\/product\//
-
 // Set pathname were middleware will be executed
 export const config = {
-  pathname: '/product',
+  matcher: '/product/:path',
 }
 
 export async function middleware(req: NextRequest) {
   // Get the product id from the URL
   const url = req.nextUrl.clone()
-
-  // Skip public files
-  if (
-    PUBLIC_FILE.test(url.href) ||
-    NEXT_FILES.test(url.href) ||
-    !MATCHED_PATHS.test(url.pathname)
-  )
-    return NextResponse.next()
 
   // Extract id from pathname
   const [, , id] = url.pathname.split('/')
