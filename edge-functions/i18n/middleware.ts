c16f1d53556803bf157cfce0c77488c265693483
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-08 14:33:55 -0400

    use matcher

--- edge-functions/i18n/middleware.ts
+++ edge-functions/i18n/middleware.ts
@@ -1,15 +1,13 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextRequest, NextResponse } from 'next/server'
 
-const PUBLIC_FILE = /\.(.*)$/
+export const config = { matching: /\.(.*)$/ }
 
 export function middleware(req: NextRequest) {
   const country = req.geo.country?.toLowerCase() || 'us'
   const locale = req.headers.get('accept-language')?.split(',')?.[0] || 'en-US'
 
   // Only rewrite files that don't have a file extension
-  if (!PUBLIC_FILE.test(req.nextUrl.pathname)) {
-    req.nextUrl.pathname = `/${locale}/${country}`
-    return NextResponse.rewrite(req.nextUrl)
-  }
+  req.nextUrl.pathname = `/${locale}/${country}`
+  return NextResponse.rewrite(req.nextUrl)
 }
