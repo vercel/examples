5e333116eae60242240d08faf325f5c4a4eb8061
goncy <gonzalo.pozzo4@gmail.com>
2022-06-13 15:32:41 -0300

    remove url clone

--- edge-functions/power-parity-pricing/middleware.ts
+++ edge-functions/power-parity-pricing/middleware.ts
@@ -6,15 +6,12 @@ export const config = {
 }
 
 export function middleware(req: NextRequest) {
-  // Clone URL
-  const url = req.nextUrl.clone()
-
   // Get country
   const country = req.geo.country?.toLowerCase() || 'us'
 
   // Update pathname
-  url.pathname = `/${country}`
+  req.nextUrl.pathname = `/${country}`
 
   // Rewrite to URL
-  return NextResponse.rewrite(url)
+  return NextResponse.rewrite(req.nextUrl)
 }
