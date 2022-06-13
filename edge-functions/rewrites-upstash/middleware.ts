140dd6f7af7140791db1427afd396a31f2ac2730
goncy <gonzalo.pozzo4@gmail.com>
2022-06-13 15:41:44 -0300

    remove url clone

--- edge-functions/rewrites-upstash/middleware.ts
+++ edge-functions/rewrites-upstash/middleware.ts
@@ -7,18 +7,17 @@ export const config = {
 }
 
 export async function middleware(req: NextRequest) {
-  // Get the product id from the URL
-  const url = req.nextUrl.clone()
-
   // Extract id from pathname
-  const [, , id] = url.pathname.split('/')
+  const [, , id] = req.nextUrl.pathname.split('/')
 
   // Check on upstash if we have stock
   const hasStock = await api.cache.get(id)
 
   // Rewrite to the correct url
-  url.pathname = hasStock ? `/product/${id}/` : `/product/${id}/no-stock`
+  req.nextUrl.pathname = hasStock
+    ? `/product/${id}/`
+    : `/product/${id}/no-stock`
 
   // Return rewrited path
-  return NextResponse.rewrite(url)
+  return NextResponse.rewrite(req.nextUrl)
 }
