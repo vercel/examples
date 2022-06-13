9abb708fe6a56e30dbc1c46a3294a5d4fede2552
goncy <gonzalo.pozzo4@gmail.com>
2022-06-13 15:44:00 -0300

    remove url clone

--- edge-functions/maintenance-page/middleware.ts
+++ edge-functions/maintenance-page/middleware.ts
@@ -6,20 +6,17 @@ export const config = {
 }
 
 export async function middleware(req: NextRequest) {
-  // Clone the URL
-  const url = req.nextUrl.clone()
-
   // Filter unwanted paths
-  if (url.pathname !== '/big-promo') return NextResponse.next()
+  if (req.nextUrl.pathname !== '/big-promo') return NextResponse.next()
 
   // Simulate connection with a redis cache
   const isInMaintenanceMode = Math.random() >= 0.5
 
   // If is in maintenance mode, point the url pathname to the maintenance page
   if (isInMaintenanceMode) {
-    url.pathname = `/maintenance`
+    req.nextUrl.pathname = `/maintenance`
 
     // Rewrite to the url
-    return NextResponse.rewrite(url)
+    return NextResponse.rewrite(req.nextUrl)
   }
 }
