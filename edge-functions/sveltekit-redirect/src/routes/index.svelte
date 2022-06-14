49fb0e2bea8ea4d656053c8553cc425ffb0cf13c
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-14 15:23:37 -0400

    vercel build

--- edge-functions/sveltekit-redirect/src/routes/index.svelte
+++ edge-functions/sveltekit-redirect/src/routes/index.svelte
@@ -1,1 +1,9 @@
-<h1>Maintenance page</h1>
+<script lang="ts">
+	import { Text, Link } from '@vercel/examples-ui-svelte';
+</script>
+
+<Text variant="h1">Vercel Middleware + Sveltekit</Text>
+
+<Text>
+	This page should reditect you to <Link href="/maintenance">maintenance</Link>
+</Text>
--- edge-functions/sveltekit-redirect/src/routes/maintenance.svelte
+++ edge-functions/sveltekit-redirect/src/routes/maintenance.svelte
@@ -1,2 +1,33 @@
-<h1>About page</h1>
-;
+<script lang="ts">
+	import { Text, Link, Code, Snippet } from '@vercel/examples-ui-svelte';
+</script>
+
+<section class="space-y-4">
+	<Text variant="h1">Sveltekit Maintenance page</Text>
+	<Text
+		>In order to create a redirect to a maintenance page, all you have to do is create a <Code
+			>middleware.ts</Code
+		> file at the root of your project with the following content:</Text
+	>
+	<pre class="snippet"><code
+			>{`import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+	if (request.nextUrl.pathname === '/') {
+		return NextResponse.redirect(new URL('/maintenance', request.url));
+	}
+}`}</code
+		></pre>
+</section>
+
+<style>
+	.snippet {
+		border-color: var(--accents-2);
+		border-width: 1px;
+		border-radius: 0.375rem;
+		background-color: var(--accents-1);
+		overflow-x: auto;
+		padding: 1.5rem;
+	}
+</style>
