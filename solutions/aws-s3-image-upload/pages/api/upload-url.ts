f21468dd440359c68574ff6414c65d580448b3fb
Lee Robinson <lrobinson2011@gmail.com>
2022-06-15 09:34:43 -0500

    Simplify AWS S3 example.

--- solutions/aws-s3-image-upload/pages/api/upload-url.ts
+++ solutions/aws-s3-image-upload/pages/api/upload-url.ts
@@ -1,18 +1,16 @@
-import aws from 'aws-sdk'
+import S3 from 'aws-sdk/clients/s3'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  aws.config.update({
+  const s3 = new S3({
+    apiVersion: '2006-03-01',
     accessKeyId: process.env.ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
-    region: process.env.REGION,
-    signatureVersion: 'v4',
   })
 
-  const s3 = new aws.S3()
   const post = await s3.createPresignedPost({
     Bucket: process.env.BUCKET_NAME,
     Fields: {
