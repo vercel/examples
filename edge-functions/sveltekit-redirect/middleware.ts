29c02e780ba1ffc6ad89433c40990dccdc5bd2e7
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-14 20:04:29 -0400

    fix

--- edge-functions/sveltekit-redirect/middleware.ts
+++ edge-functions/sveltekit-redirect/middleware.ts
@@ -2,4 +2,11 @@ export default function middleware(request) {
 	if (new URL(request.url).pathname === '/') {
 		return Response.redirect(new URL('/maintenance', request.url));
 	}
+
+	return new Response(null, {
+		headers: {
+		  'x-middleware-next': '1',
+		},
+	  });
+	 
 }
