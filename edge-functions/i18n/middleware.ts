31b976e019a542e0652e7b2dd295cbbb32b44c8c
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-15 14:12:03 -0400

    include matcher and more comments

--- edge-functions/i18n/middleware.ts
+++ edge-functions/i18n/middleware.ts
@@ -1,13 +1,13 @@
-// eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextRequest, NextResponse } from 'next/server'
 
-// export const config = {
-//   matcher: '/',
-// }
+// only run middleware on home page
+export const config = {
+  matcher: '/',
+}
 
-export function middleware(req: NextRequest) {
-  console.log('running')
-  const country = req.geo.country?.toLowerCase() || 'us'
+export default function middleware(req: NextRequest) {
+  // req.geo is only available on pro and enterprise accounts
+  const country = req.geo?.country?.toLowerCase() || 'us'
   const locale = req.headers.get('accept-language')?.split(',')?.[0] || 'en-US'
 
   // Only rewrite files that don't have a file extension
