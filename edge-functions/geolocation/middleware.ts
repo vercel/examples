79fdaa5c4702b2900b0fea4cc5b79a672416621b
Dom Sip <dom@vercel.com>
2022-06-16 12:22:09 +0100

    refactor: update geo example to use new middleware (#285)

--- edge-functions/geolocation/middleware.ts
+++ edge-functions/geolocation/middleware.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @next/next/no-server-import-in-page */
 import { NextRequest, NextResponse } from 'next/server'
-import countries from '../lib/countries.json'
+import countries from './lib/countries.json'
+
+// run only on homepage
+export const config = {
+  matcher: '/',
+}
 
 export async function middleware(req: NextRequest) {
   const { nextUrl: url, geo } = req
