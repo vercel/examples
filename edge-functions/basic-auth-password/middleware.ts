f3a821ca6277c0af563811641a09d2e863675496
Luis Alvarez D <luis@vercel.com>
2022-06-16 13:01:47 -0500

    Update edge-functions/basic-auth-password/middleware.ts

--- edge-functions/basic-auth-password/middleware.ts
+++ edge-functions/basic-auth-password/middleware.ts
@@ -16,7 +16,7 @@ export function middleware(req: NextRequest) {
       return NextResponse.next()
     }
   }
-  url.pathname = `api/auth`
+  url.pathname = '/api/auth'
 
   return NextResponse.rewrite(url)
 }
