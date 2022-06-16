bf8cb74afd1359c7a042aaf6d5ea01aa974ae6c1
Dom Sip <dom@vercel.com>
2022-06-16 15:05:01 +0100

    refactor: update basic auth example to use new middleware (#286)

--- edge-functions/basic-auth-password/middleware.ts
+++ edge-functions/basic-auth-password/middleware.ts
@@ -1,21 +1,24 @@
+/* eslint-disable @next/next/no-server-import-in-page */
 import { NextRequest, NextResponse } from 'next/server'
+
+// limit middleware to only home page
+export const config = {
+  matcher: '/',
+}
 
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(auth).split(":")
+    const [user, pwd] = atob(auth).split(':')
 
     if (user === '4dmin' && pwd === 'testpwd123') {
       return NextResponse.next()
     }
   }
+  url.pathname = `/auth`
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  })
+  return NextResponse.rewrite(url)
 }
