11248f55301ede79ba003011b3ea377f05111e27
Bel Curcio <curciobel@gmail.com>
2022-06-16 19:51:51 +0200

    Updates

--- edge-functions/basic-auth-password/middleware.ts
+++ edge-functions/basic-auth-password/middleware.ts
@@ -1,17 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// limit middleware to only home page
 export const config = {
   matcher: '/',
 }
 
 export function middleware(req: NextRequest) {
-  const authValue = req.headers.get('authorization')
+  const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
-  if (authValue) {
-    const auth = authValue.split(' ')[1]
-    const [user, pwd] = atob(auth).split(':')
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
 
     if (user === '4dmin' && pwd === 'testpwd123') {
       return NextResponse.next()
