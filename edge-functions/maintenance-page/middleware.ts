2bcac98aab5a08a1170fa97d4f09a409daec4ab3
Gonzalo Pozzo <gonzalo.pozzo4@gmail.com>
2022-06-17 00:54:07 -0300

    Update maintenance-page example (#278)
    * Remove old middleware file and update next version
    
    * Add new middleware file
    
    * update to use matching
    
    * remove url clone
    
    * fix feedback
    
    * fix feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: Luis Alvarez D <luis@vercel.com>

--- edge-functions/maintenance-page/middleware.ts
+++ edge-functions/maintenance-page/middleware.ts
@@ -1,17 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest) {
-  // Clone the URL
-  const url = req.nextUrl.clone()
+export const config = {
+  matcher: '/big-promo',
+}
 
+export async function middleware(req: NextRequest) {
   // Simulate connection with a redis cache
   const isInMaintenanceMode = Math.random() >= 0.5
 
   // If is in maintenance mode, point the url pathname to the maintenance page
   if (isInMaintenanceMode) {
-    url.pathname = `/maintenance`
+    req.nextUrl.pathname = `/maintenance`
 
     // Rewrite to the url
-    return NextResponse.rewrite(url)
+    return NextResponse.rewrite(req.nextUrl)
   }
 }
