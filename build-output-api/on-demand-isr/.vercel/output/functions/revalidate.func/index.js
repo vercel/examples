466d675aa4a69470e6643e19a1a130bc795c5c26
Sean Massa <endangeredmassa@gmail.com>
2022-06-21 10:34:02 -0500

    remove ";"

--- build-output-api/on-demand-isr/.vercel/output/functions/revalidate.func/index.js
+++ build-output-api/on-demand-isr/.vercel/output/functions/revalidate.func/index.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const https = require('https')
 
 // The bypass token should be a build-time randomly generated string of at least 32 characters.
 // This value should only be exposed in the function config and inside of the function itself.
@@ -6,32 +6,32 @@ const https = require('https');
 const bypassToken = '87734ad8259d67c3c11747d3e4e112d0'
 
 module.exports = (req, res) => {
-  const host = req.headers.host;
-  const deployedUrl = `https://${host}`;
+  const host = req.headers.host
+  const deployedUrl = `https://${host}`
 
   const options = {
     hostname: host,
     port: 443,
     path: '/',
-    method: 'GET', // MUST be "GET"; a "HEAD" or "POST" request will not work
+    method: 'GET', // MUST be "GET" a "HEAD" or "POST" request will not work
     headers: {
       'x-prerender-revalidate': bypassToken
     }
-  };
+  }
   const revalidateRequest = https.request(options, revalidateResponse => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.setHeader('Content-Type', 'text/html charset=utf-8')
     res.end(`
 <h1>Cache Revalidated!</h1>
 <p>Redirecting you back.</p>
-<meta http-equiv="refresh" content="2; url=${deployedUrl}">
+<meta http-equiv="refresh" content="2 url=${deployedUrl}">
 `)
-  });
-  
+  })
+
   revalidateRequest.on('error', error => {
-    console.error(error.stack);
+    console.error(error.stack)
     res.statusCode = 500
     res.end()
-  });
+  })
 
-  revalidateRequest.end();
+  revalidateRequest.end()
 }
