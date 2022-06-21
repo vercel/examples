aee409ca8185545fcb165e614305e7919a8fd054
Sean Massa <EndangeredMassa@gmail.com>
2022-06-21 14:01:08 -0500

    Update build-output-api/on-demand-isr/.vercel/output/functions/index.func/index.js
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- build-output-api/on-demand-isr/.vercel/output/functions/index.func/index.js
+++ build-output-api/on-demand-isr/.vercel/output/functions/index.func/index.js
@@ -1,6 +1,8 @@
 module.exports = (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
+  // NOTE: the below revalidate link is meant for demo purposes only
+  // actual authentication should be used in actual projects
   res.end(`
 <h1>On-demand Incremental Static Regeneration (ISR) Example</h1>
 <p>The "server time" below will only get updated after you click the Revalidate link.</p>
