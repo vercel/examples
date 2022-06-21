cc288005a7f936f1d8463c0aa8a9455884f62000
Sean Massa <endangeredmassa@gmail.com>
2022-06-21 13:25:09 -0500

    add trivial auth

--- build-output-api/on-demand-isr/.vercel/output/functions/index.func/index.js
+++ build-output-api/on-demand-isr/.vercel/output/functions/index.func/index.js
@@ -5,6 +5,6 @@ module.exports = (req, res) => {
 <h1>On-demand Incremental Static Regeneration (ISR) Example</h1>
 <p>The "server time" below will only get updated after you click the Revalidate link.</p>
 <p>Server time: ${new Date().toISOString()}</p>
-<p><a href="/revalidate">Revalidate</a></p>
+<p><a href="/revalidate?authToken=a13f94f6-a441-47ca-95fc-9f44f3450295">Revalidate</a></p>
 `)
 }
--- build-output-api/on-demand-isr/.vercel/output/functions/revalidate.func/index.js
+++ build-output-api/on-demand-isr/.vercel/output/functions/revalidate.func/index.js
@@ -2,10 +2,20 @@ const https = require('https')
 
 // The bypass token should be a build-time randomly generated string of at least 32 characters.
 // This value should only be exposed in the function config and inside of the function itself.
-// DO NOT expose this value on the client-side or a malicious user could trigger boundless revalidations
+// DO NOT expose this value on the client-side: a malicious user could trigger boundless revalidations.
 const bypassToken = '87734ad8259d67c3c11747d3e4e112d0'
 
+// This authToken is a trivial example of authentication, but any strong authentication method will work.
+// DO NOT allow unauthenticated users to trigger revalidation: a malicious user could trigger boundless revalidations.
+const authToken = 'a13f94f6-a441-47ca-95fc-9f44f3450295';
+
 module.exports = (req, res) => {
+  const params = new URLSearchParams(req.query);
+  if (authToken !== params.get('authToken')) {
+    res.statusCode = 403
+    res.end('Not Authorized')
+  }
+
   const host = req.headers.host
   const deployedUrl = `https://${host}`
 
@@ -13,7 +23,7 @@ module.exports = (req, res) => {
     hostname: host,
     port: 443,
     path: '/',
-    method: 'GET', // MUST be "GET" a "HEAD" or "POST" request will not work
+    method: 'GET', // MUST be "GET"; a "HEAD" or "POST" request will not work
     headers: {
       'x-prerender-revalidate': bypassToken
     }
