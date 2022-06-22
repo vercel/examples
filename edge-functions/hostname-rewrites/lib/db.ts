d4ac39407600fb202f48e3a8e93cf377229234fb
goncy <gonzalo.pozzo4@gmail.com>
2022-06-22 15:57:42 -0300

    Add db file

---
+++ edge-functions/hostname-rewrites/lib/db.ts
@@ -0,0 +1,22 @@
+const mockDB = [
+  {
+    name: 'Site 1',
+    description: 'Subdomain + custom domain',
+    subdomain: 'subdomain-1',
+    customDomain: 'custom-domain-1.com',
+  },
+  {
+    name: 'Site 2',
+    description: 'Subdomain only',
+    subdomain: 'subdomain-2',
+    customDomain: null,
+  },
+  {
+    name: 'Site 3',
+    description: 'Subdomain only',
+    subdomain: 'subdomain-3',
+    customDomain: null,
+  },
+]
+
+export default mockDB
