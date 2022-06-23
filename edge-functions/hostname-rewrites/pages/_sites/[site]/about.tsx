1421f03a84a6d177f6f3394193790fab2aeaee88
Gonzalo Pozzo <gonzalo.pozzo4@gmail.com>
2022-06-23 11:35:27 -0300

    Update hostname-rewrites example (#316)
    * update old middleware references
    
    * add npmrc
    
    * update versions
    
    * update cr feedback
    
    * Add db file
    
    * Updated middleware implementation of hostnames
    
    * Removed request of ROOT_DOMAIN
    
    * Fixed bug with preview
    
    * Fix for localhost
    
    * change matcher for /
    
    * undo path change
    
    Co-authored-by: LFades <luis@vercel.com>

--- edge-functions/hostname-rewrites/pages/_sites/[site]/about.tsx
+++ edge-functions/hostname-rewrites/pages/_sites/[site]/about.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { Layout, Page, Text, Link } from '@vercel/examples-ui'
 
+import { getHostnameDataBySubdomain, getSubdomainPaths } from '../../../lib/db'
+
 export default function About(props) {
   const router = useRouter()
   if (router.isFallback) {
@@ -16,11 +18,6 @@ export default function About(props) {
 
   return (
     <Page>
-      <Head>
-        <title>{props.name}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta itemProp="description" content={props.description} />
-      </Head>
       <Text variant="h1" className="mb-6">
         About {props.name}
       </Text>
@@ -45,60 +42,16 @@ export default function About(props) {
 
 About.Layout = Layout
 
-const mockDB = [
-  {
-    name: 'Site 1',
-    description: 'Subdomain + custom domain',
-    subdomain: 'subdomain-1',
-    customDomain: 'custom-domain-1.com',
-  },
-  {
-    name: 'Site 2',
-    description: 'Subdomain only',
-    subdomain: 'subdomain-2',
-    customDomain: null,
-  },
-  {
-    name: 'Site 3',
-    description: 'Subdomain only',
-    subdomain: 'subdomain-3',
-    customDomain: null,
-  },
-]
-
 export async function getStaticPaths() {
-  // get all sites that have subdomains set up
-  const subdomains = mockDB.filter((item) => item.subdomain)
-
-  // get all sites that have custom domains set up
-  const customDomains = mockDB.filter((item) => item.customDomain)
-
-  // build paths for each of the sites in the previous two lists
-  const paths = [
-    ...subdomains.map((item) => {
-      return { params: { site: item.subdomain } }
-    }),
-    ...customDomains.map((item) => {
-      return { params: { site: item.customDomain } }
-    }),
-  ]
   return {
-    paths: paths,
+    paths: await getSubdomainPaths(),
     fallback: true, // fallback true allows sites to be generated using ISR
   }
 }
 
 export async function getStaticProps({ params: { site } }) {
-  // check if site is a custom domain or a subdomain
-  const customDomain = site.includes('.') ? true : false
-
-  // fetch data from mock database using the site value as the key
-  const data = mockDB.filter((item) =>
-    customDomain ? item.customDomain == site : item.subdomain == site
-  )
-
   return {
-    props: { ...data[0] },
-    revalidate: 10, // set revalidate interval of 10s
+    props: await getHostnameDataBySubdomain(site),
+    revalidate: 3600, // set revalidate interval of 1 hour
   }
 }
