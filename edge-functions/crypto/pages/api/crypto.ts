9c39aa1bbec372764c023b8b5272292e87998ef6
Bel Curcio <curciobel@gmail.com>
2022-06-23 15:18:10 +0200

    updates

--- edge-functions/crypto/pages/api/crypto.ts
+++ edge-functions/crypto/pages/api/crypto.ts
@@ -3,12 +3,14 @@
 // ------------------
 
 export default async function CryptoEdgeAPIRoute() {
-  // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/subtle
   const plainText = 'Hello from the Edge!'
   const password = 'hunter2'
   const ptUtf8 = new TextEncoder().encode(plainText)
   const pwUtf8 = new TextEncoder().encode(password)
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/subtle
   const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8)
+
   // Encrypt
   const iv = crypto.getRandomValues(new Uint8Array(12))
   const alg = { name: 'AES-GCM', iv: iv }
@@ -16,12 +18,14 @@ export default async function CryptoEdgeAPIRoute() {
     'encrypt',
   ])
   const encrypted = await crypto.subtle.encrypt(alg, encrpytKey, ptUtf8)
+
   // Decrypt
   const decryptKey = await crypto.subtle.importKey('raw', pwHash, alg, false, [
     'decrypt',
   ])
   const ptBuffer = await crypto.subtle.decrypt(alg, decryptKey, encrypted)
   const decryptedText = new TextDecoder().decode(ptBuffer)
+
   return new Response(
     JSON.stringify({
       // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/randomUUID
