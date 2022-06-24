798579d8159598e8862e601b274b84abf5648165
Bel Curcio <curciobel@gmail.com>
2022-06-24 09:05:41 +0200

    Revert "Update hostname-rewrites example (#316)"
    This reverts commit 1421f03a84a6d177f6f3394193790fab2aeaee88.

---
+++ edge-functions/hostname-rewrites/pages/_middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone()
+  // Get hostname (e.g. vercel.com, test.vercel.app, etc.)
+  const hostname = req.headers.get('host')
+
+  // If localhost, assign the host value manually
+  // If prod, get the custom domain/subdomain value by removing the root URL
+  // (in the case of "test.vercel.app", "vercel.app" is the root URL)
+  const currentHost =
+    process.env.NODE_ENV == 'production'
+      ? hostname.replace(`.${process.env.ROOT_URL}`, '')
+      : process.env.CURR_HOST
+
+  // Prevent security issues – users should not be able to canonically access
+  // the pages/sites folder and its respective contents. This can also be done
+  // via rewrites to a custom 404 page
+  if (url.pathname.startsWith(`/_sites`)) {
+    return new Response(null, { status: 404 })
+  }
+
+  if (
+    !url.pathname.includes('.') && // exclude all files in the public folder
+    !url.pathname.startsWith('/api') // exclude all API routes
+  ) {
+    // rewrite to the current hostname under the pages/sites folder
+    // the main logic component will happen in pages/sites/[site]/index.tsx
+    url.pathname = `/_sites/${currentHost}${url.pathname}`
+    return NextResponse.rewrite(url)
+  }
+}
