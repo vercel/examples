b7bb8b4e3a0e5e1a398659fba5b7f9bf9a5735a0
Bel Curcio <curciobel@gmail.com>
2022-06-24 10:48:32 +0200

    Code Conventions/Styling

--- edge-functions/crypto/middleware.ts
+++ edge-functions/crypto/middleware.ts
@@ -1,9 +1,15 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextResponse } from 'next/server'
+// eslint-disable-next-line @next/next/no-server-import-in-page
 import type { NextRequest } from 'next/server'
 
 // ------------------
 // Using Crypto with Edge Middleware
 // ------------------
+
+export const config = {
+  matcher: '/',
+}
 
 export function middleware(request: NextRequest) {
   const token = crypto.randomUUID()
@@ -11,8 +17,4 @@ export function middleware(request: NextRequest) {
   url.pathname = '/api/crypto'
   url.searchParams.set('token', token)
   return NextResponse.redirect(url)
-}
-
-export const config = {
-  matcher: '/',
 }
--- edge-functions/crypto/pages/index.tsx
+++ edge-functions/crypto/pages/index.tsx
@@ -1,3 +1,4 @@
+// This page is set as fallback. It won't be served unless middleware's matcher changes.
 export default function Index() {
   return <div>Example using Crypto API</div>
 }
