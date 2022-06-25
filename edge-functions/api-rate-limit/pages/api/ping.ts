01d5d53613e78ab682c040f2f2a78711ae879804
LFades <luis@vercel.com>
2022-06-25 15:26:43 -0500

    Removed middleware

--- edge-functions/api-rate-limit/pages/api/ping.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  // Get the amount of requests remaining.
-  const remainingRequests = res.getHeader('X-RateLimit-Remaining')
-
-  // Return `success` being a boolean indicating if we have reached the rate limit.
-  return res.json({ success: Number(remainingRequests) > 0 })
-}
