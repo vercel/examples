08250e0a28c6b7c2f53051530141d6b0e81b12c3
Lee Robinson <lrobinson2011@gmail.com>
2022-06-25 16:55:31 -0500

    Another one

--- edge-api-routes/cache-control/api/edge.ts
+++ edge-api-routes/cache-control/api/edge.ts
@@ -2,10 +2,17 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
-// http://localhost:3000/api/edge?email=test@test.com
-
 export default async function handler(req) {
-  const { searchParams } = new URL(req.url)
-  const email = searchParams.get('email')
-  return new Response(email)
+  return new Response(
+    JSON.stringify({
+      message: 'Hello, world!',
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+        'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+      },
+    }
+  )
 }
