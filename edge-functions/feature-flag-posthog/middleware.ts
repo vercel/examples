d1eb5ad482213b9451484449499ed98f03f71409
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-27 08:20:38 -0400

    wip

---
+++ edge-functions/feature-flag-posthog/middleware.ts
@@ -0,0 +1,63 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextRequest, NextResponse } from 'next/server'
+import { DISTINCT_ID_COOKIE_NAME, FEATURE_FLAGS } from '@lib/constants'
+import { getFeatureFlagVariant, isFeatureFlagEnabled } from '@lib/posthog-node'
+
+export const config = {
+  matcher: ['/product(.*)', '/about(.*)', '/marketing(.*)'],
+}
+
+export default async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname
+
+  if (pathname.includes('/product')) {
+    console.log('product')
+    const productVariantValue = await getFeatureFlagVariant(
+      req.cookies.get(DISTINCT_ID_COOKIE_NAME),
+      FEATURE_FLAGS.NEW_PRODUCT_PAGE
+    )
+
+    // redirect to the path based on the variant value
+    return NextResponse.rewrite(
+      new URL(`/product/${productVariantValue}`, req.url)
+    )
+  }
+
+  if (pathname.includes('/marketing')) {
+    console.log('marketing')
+    // Redirect paths that go directly to the variant
+    if (pathname != '/marketing') {
+      return NextResponse.redirect(new URL('/marketing', req.url))
+    }
+
+    const newMarketingPageFlagEnabled = await isFeatureFlagEnabled(
+      req.cookies[DISTINCT_ID_COOKIE_NAME],
+      FEATURE_FLAGS.NEW_MARKETING_PAGE
+    )
+
+    return NextResponse.rewrite(
+      new URL(
+        newMarketingPageFlagEnabled ? '/marketing/b' : '/marketing',
+        req.url
+      )
+    )
+  }
+
+  if (pathname.includes('/about')) {
+    console.log('about')
+
+    // Redirect paths that go directly to the variant
+    if (pathname != '/about') {
+      return NextResponse.redirect(new URL('/about', req.url))
+    }
+
+    const newAboutPageFlagEnabled = await isFeatureFlagEnabled(
+      req.cookies[DISTINCT_ID_COOKIE_NAME],
+      FEATURE_FLAGS.NEW_ABOUT_PAGE
+    )
+
+    return NextResponse.rewrite(
+      new URL(newAboutPageFlagEnabled ? '/about/b' : '/about', req.url)
+    )
+  }
+}
