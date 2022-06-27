55d6da3590d8465d7055ca2bace4bb2864c98956
Steven Tey <stevensteel97@gmail.com>
2022-06-27 09:06:02 -0700

    Update middleware.ts

--- edge-functions/rewrite-to-external-url/middleware.ts
+++ edge-functions/rewrite-to-external-url/middleware.ts
@@ -3,15 +3,14 @@ import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
-  const hostname = req.headers.get('host')
+  const url = req.nextUrl
 
-  if (hostname === 'support-site.vercel.pub' || hostname === 'localhost:3000') {
+  if (url.hostname === 'support-site.vercel.pub' || url.hostname === 'localhost:3000') {
     // rewrites "support-site.vercel.pub" to "https://www.zendesk.com"
     // note that it's important to check if the URL that you are rewriting to
     // has trailing slashes or not – or else you'll end up with a redirect loop.
     // also, note that the URL that you are rewriting to must be a full URL,
     // and has to include the `www` subdomain if it's present.
-    return NextResponse.rewrite(`https://www.zendesk.com/${pathname}/`)
+    return NextResponse.rewrite(`https://www.zendesk.com/${url.pathname}/`)
   }
 }
