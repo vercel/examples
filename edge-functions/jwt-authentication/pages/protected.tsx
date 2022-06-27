4ce37555b67587ece824aa3ebedf9095b0193e7b
LFades <luis@vercel.com>
2022-06-27 09:16:02 -0500

    Updated wording in the example

--- edge-functions/jwt-authentication/pages/protected.tsx
+++ edge-functions/jwt-authentication/pages/protected.tsx
@@ -1,8 +1,8 @@
 import { Layout } from '@vercel/examples-ui'
-import { Router, useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { Page, Text, Code, Link, Button } from '@vercel/examples-ui'
 
-import { invalidateAuth, requestAuth } from '@lib/auth'
+import { invalidateAuth } from '@lib/auth'
 import { USER_TOKEN } from '@lib/constants'
 
 export default function Protected() {
@@ -10,19 +10,18 @@ export default function Protected() {
   const handleRemoveCookie = () => {
     invalidateAuth().then(() => reload())
   }
+
   return (
     <Page>
-      <div className="text-center mb-6">
-        <Text variant="h1" className="mb-4">
-          JWT Authentication (Protected page)
-        </Text>
-      </div>
+      <Text variant="h1" className="mb-6">
+        JWT Authentication (Protected page)
+      </Text>
       <section className="space-y-4">
         <Text>
           This page is protected, you cannot reach it unless you have the{' '}
           <Code>{USER_TOKEN}</Code> JWT Cookie. If you remove the cookie, the
-          next time you visit this page, you will be redirected to the index
-          page <Code>/</Code>.
+          next time you visit this page, you will be redirected to the{' '}
+          <Link href="/">index page</Link>.
         </Text>
         <Button onClick={handleRemoveCookie}>Remove cookie and Reload</Button>
       </section>
