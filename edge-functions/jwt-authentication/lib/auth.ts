4b1d30269bf976cf1c0af82d7741bafabb4ffe30
LFades <luis@vercel.com>
2022-06-27 09:29:13 -0500

    Updated verifyAuth

--- edge-functions/jwt-authentication/lib/auth.ts
+++ edge-functions/jwt-authentication/lib/auth.ts
@@ -10,15 +10,15 @@ interface UserJwtPayload {
   iat: number
 }
 
+export class AuthError extends Error {}
+
 /**
  * Verifies the user's JWT token and returns its payload if it's valid.
  */
 export async function verifyAuth(request: NextRequest) {
   const token = request.cookies.get(USER_TOKEN)
 
-  if (!token) {
-    return null
-  }
+  if (!token) throw new AuthError('Missing auth token')
 
   try {
     const verified = await jwtVerify(
@@ -27,8 +27,7 @@ export async function verifyAuth(request: NextRequest) {
     )
     return verified.payload as UserJwtPayload
   } catch (err) {
-    console.error('Your token has expired.')
-    return null
+    throw new AuthError('Your token has expired.')
   }
 }
 
--- edge-functions/jwt-authentication/middleware.ts
+++ edge-functions/jwt-authentication/middleware.ts
@@ -1,17 +1,16 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { type NextRequest, NextResponse } from 'next/server'
 import { verifyAuth } from '@lib/auth'
-import { USER_TOKEN } from '@lib/constants'
 
 export const config = {
   matcher: '/protected',
 }
 
 export async function middleware(req: NextRequest, res: NextResponse) {
-  const token = req.cookies.get(USER_TOKEN)
-
   // validate the user is authenticated
-  const verifiedToken = await verifyAuth(req)
+  const verifiedToken = await verifyAuth(req).catch((err) => {
+    console.error(err.message)
+  })
 
   // redirect if the token is invalid
   if (!verifiedToken) {
