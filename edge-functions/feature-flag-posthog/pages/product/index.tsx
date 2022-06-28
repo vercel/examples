0e6b0708b588df2628b443688f739d57e5e70a4a
LFades <luis@vercel.com>
2022-06-27 23:34:59 -0500

    Removed unused product pages

--- edge-functions/feature-flag-posthog/pages/product/index.tsx
+++ edge-functions/feature-flag-posthog/pages/product/index.tsx
@@ -1,16 +1,14 @@
-import { Layout, Page, Text, Code, Link } from '@vercel/examples-ui'
+import { Layout, Page, Text, Link } from '@vercel/examples-ui'
 
 export default function Product() {
   return (
     <Page>
       <Text variant="h2" className="mb-6">
-        Product Default Variant
+        Product page
       </Text>
-      <Text className="text-lg mb-4">
-        This is a fallback. Check middleware.ts&apos;s logic and that
-        PostHog&apos;s SDK correctly set the cookie. You&apos;re currently
-        looking at the variant of the product page under{' '}
-        <Code>pages/product/index.tsx</Code>
+      <Text className="text-lg mb-4">This is the original marketing page</Text>
+      <Text className="mb-4">
+        You&apos;re currently on <b>/marketing</b>
       </Text>
       <Link href="/">Go back to /</Link>
     </Page>
