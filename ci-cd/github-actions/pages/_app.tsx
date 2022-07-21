2d8ab6d922475d5511397428fda8ec4c011034ff
Jason Wiker <Jason.wiker@vercel.com>
2022-07-20 21:42:45 -0600

    Update to a basic index.html

--- ci-cd/github-actions/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app'
-
-import '@vercel/examples-ui/globals.css'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default App
