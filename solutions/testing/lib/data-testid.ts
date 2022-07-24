4e06962e902a8c45cc03ffb3761dc895877a141c
LFades <luis@vercel.com>
2022-07-24 18:26:03 -0500

    Added data-testid

---
+++ solutions/testing/lib/data-testid.ts
@@ -0,0 +1,42 @@
+import sluga from 'sluga'
+
+const SEP = '/'
+
+const slugifyCache = new Map<string, string>()
+
+/**
+ * Generates a slug for based on the given string.
+ *
+ * @param str - The string to slugify
+ * @returns
+ */
+function slugify(str: string) {
+  let val = slugifyCache.get(str)
+
+  if (!val) {
+    val = sluga(str)
+    slugifyCache.set(str, val)
+  }
+
+  return val
+}
+
+/**
+ * Generates a test id that can be used in `data-testid` attributes.
+ *
+ * @param scopes - descriptions of the scopes that'll form the test id
+ * @returns A valid `data-testid` value
+ */
+function generateTestId(...scopes: (string | string[])[]): string {
+  // We create the test id in one loop to avoid the JS impact of adding
+  // test ids everywhere in pages.
+  return scopes.reduce<string>((tid, scope) => {
+    if (!scope.length) return tid
+    if (Array.isArray(scope)) {
+      return generateTestId(tid, ...scope)
+    }
+    return `${tid}${SEP}${slugify(scope)}`
+  }, '')
+}
+
+export const tid = generateTestId
