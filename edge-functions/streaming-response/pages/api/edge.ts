436152cc751025ed0135d863865374503c55ae9e
Bel Curcio <curciobel@gmail.com>
2022-08-15 14:57:38 +0200

    Testing Streams

--- edge-functions/streaming-response/pages/api/edge.ts
+++ edge-functions/streaming-response/pages/api/edge.ts
@@ -1,27 +1,34 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
 export const config = {
   runtime: 'experimental-edge',
 }
 
-// Streamable content
-const RESOURCE_URL = "https://gist.githubusercontent.com/okbel/8ba642143f6912548df2d79f2c0ebabe/raw/4bcf9dc5750b42fa225cf6571d6aaa68c23a73aa/README.md"
+const RESOURCE_URL =
+  'https://gist.githubusercontent.com/okbel/8ba642143f6912548df2d79f2c0ebabe/raw/4bcf9dc5750b42fa225cf6571d6aaa68c23a73aa/README.md'
 
-export default async function handler(request: NextRequest) {
-  const r = await fetch(RESOURCE_URL)
-  return new Response(r.body?.pipeThrough(transform))
+export default async function handler(req: NextRequest, res: NextResponse) {
+  const resourceRequest = await fetch(RESOURCE_URL)
+  const stream = resourceRequest.body
+  return new Response(stream.pipeThrough(transform))
 }
 
-const decoder = new TextDecoder();
+const decoder = new TextDecoder()
 const transform = new TransformStream({
-  start: (controller) => {
-    controller.enqueue(`Resource: ${RESOURCE_URL}\n\n\n\n --------------------------------- \n\n `)
+  start: (ctrl) => {
+    ctrl.enqueue(
+      `Resource: ${RESOURCE_URL}\n\n\n\n --------------------------------- \n\n `
+    )
   },
-  transform: (chunk, controller) => {
-    controller.enqueue(decoder.decode(chunk).toUpperCase());
+  transform: (chunk, ctrl) => {
+    ctrl.enqueue(decoder.decode(chunk).toUpperCase())
   },
-  flush(controller) {
-    controller.enqueue("\n\n\n\n\n\n\n\n\n\n --------------------------------- \n\n Note: This content has been transformed using Vercel's Edge Runtime and Edge Functions");
-    console.log("Done transforming. Happy streaming using Vercel's Edge Runtime and Edge Functions!")
-  }
-});
+  flush(ctrl) {
+    ctrl.enqueue(
+      "\n\n\n\n\n\n\n\n\n\n --------------------------------- \n\n Note: This content has been transformed using Vercel's Edge Runtime and Edge Functions"
+    )
+    console.log(
+      "Done transforming. Happy streaming using Vercel's Edge Runtime and Edge Functions!"
+    )
+  },
+})
