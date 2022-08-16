62e4ed03e63de29f230bf0464a14c4b54e56e2db
Bel Curcio <curciobel@gmail.com>
2022-08-16 13:56:57 +0200

    Styling

--- edge-functions/streams/pages/index.tsx
+++ edge-functions/streams/pages/index.tsx
@@ -1,26 +1,49 @@
-import {
-  Layout,
-  Page,
-  Button,
-  Input,
-  Text,
-  Link,
-  Code,
-} from '@vercel/examples-ui'
+import { Layout, Page, Button, Text } from '@vercel/examples-ui'
+import Link from 'next/link'
 
 function Index() {
   return (
     <Page>
       <div className="text-center mb-6">
-        <Text variant="h1" className="mb-4">
+        <Text variant="h2" className="mb-4">
           Streaming from the Edge with Vercel Edge Functions
         </Text>
         <Text className="mb-4">
-          With <i className="font-semibold">Vercel&apos;s Edge Middleware</i>{' '}
-          we&apos;re able to execute functions at the edge level and act on
-          mid-flight requests instantly. This example uses Upstash for API rate
-          limiting and to add rules that allows us to block certain IPs.
+          <i className="font-semibold">Vercel Edge Functions</i> allow you to
+          deliver content to your site&apos;s visitors with speed. <br /> They
+          are deployed globally by default on Vercel&apos;s Edge Network and
+          enable you to move server-side logic to the Edge, close to your
+          visitor&apos;s origin.
         </Text>
+        <div className="mt-20">
+          <div className="my-4">
+            Examples using <strong>Edge Runtime and Streams</strong>:
+            <br />{' '}
+            <span className="text-sm">
+              Find the code in{' '}
+              <pre className="inline bg-gray-100 px-1.5 rounded-sm">
+                pages/api
+              </pre>
+            </span>
+          </div>
+          <nav className="space-x-6">
+            <Link href="/api/01-simple">
+              <a>
+                <Button>Simple RS Stream</Button>
+              </a>
+            </Link>
+            <Link href="/api/02-simple-transform">
+              <a>
+                <Button>Simple RS Stream + Transform</Button>
+              </a>
+            </Link>
+            <Link href="/api/03-external-transform">
+              <a>
+                <Button>External Fetch + Transform</Button>
+              </a>
+            </Link>
+          </nav>
+        </div>
       </div>
     </Page>
   )
