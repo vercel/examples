b75c6b6a983ee2e73f4d6931ee9e0d43ac006917
Luis Alvarez D <luis@vercel.com>
2022-09-09 12:42:04 -0500

    Testing example (#376)
    ### Description
    
    This example provides all the tooling around having a powerful testing
    setup for your own apps. There are some improvements to do like adding a
    workflow for GitHub CI, moving the testing setup to a package in a
    monorepo, and adding steps in the readme about writing new tests, but
    for now it's large enough and can use a review.
    
    Looking at the readme is a good place to start, see if there's anything
    that looks wrong about the setup 👍
    
    ### Demo URL
    
    (will be added soon)
    
    ### Type of Change
    
    - [x] New Example
    - [ ] Example updates (Bug fixes, new features, etc.)
    - [ ] Other (changes to the codebase, but not to examples)
    
    ### New Example Checklist
    
    - [x] 🛫 `npm run new-example` was used to create the example
    - [ ] 📚 The template wasn't used but I carefuly read the [Adding a new
    example](https://github.com/vercel/examples#adding-a-new-example) steps
    and implemented them in the example
    - [ ] 📱 Is it responsive? Are mobile and tablets considered?

---
+++ solutions/testing/playwright/integration/setup-fixture.ts
@@ -0,0 +1,58 @@
+import { test as base } from '@playwright/test'
+import { IS_CI, PAUSE_ON_FAILURE } from 'shared/constants'
+import pauseOnFailure from 'shared/fixtures/pause-on-failure'
+import createApiMockFn from './utils/create-mock-api'
+import { createApiMocks, type MockApi } from './apis'
+import { createUtils, type Utils } from 'shared/fixtures/utils'
+
+type Extensions = { utils: Utils; mockApi: MockApi }
+
+export const test = base.extend<Extensions>({
+  context: async ({ context, baseURL }, use) => {
+    if (!baseURL) {
+      throw new Error(
+        '`baseURL` is required in order to run integration tests.'
+      )
+    }
+
+    await context.route('**/*', (route, request) => {
+      const resourceType = request.resourceType()
+      const url = request.url()
+
+      // Block any request that doesn't start with the base URL.
+      const isBlocked = !url.startsWith(baseURL)
+
+      // Block fetch/XHR requests, but not other asset types like scripts.
+      // `other` matches preloading requests that an be used in SWR.
+      const isFetchRequest = ['fetch', 'xhr', 'other'].includes(resourceType)
+
+      // Allow all HEAD requests.
+      const isHeadRequest = request.method() === 'HEAD'
+
+      // Allow requests to specific paths.
+      const isAllowed = ['/_next/'].some((allowedPath) =>
+        url.startsWith(`${baseURL}${allowedPath}`)
+      )
+
+      if (!isBlocked && (!isFetchRequest || isHeadRequest || isAllowed)) {
+        return route.continue()
+      }
+
+      console.log('Route blocked:', resourceType, url, request.method())
+
+      route.fulfill()
+    })
+
+    await use(context)
+  },
+  mockApi: ({ page }, use) => use(createApiMocks(createApiMockFn(page))),
+  utils: ({ page }, use) => use(createUtils(page)),
+})
+
+if (!IS_CI && PAUSE_ON_FAILURE) {
+  test.afterEach(pauseOnFailure)
+}
+
+export type Test = typeof test
+
+export { expect } from '@playwright/test'
