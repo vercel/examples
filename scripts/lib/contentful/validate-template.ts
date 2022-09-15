914e4e7a4bfcf81266316c17a2e8076dd4a4d74e
LFades <luis@vercel.com>
2022-09-15 17:07:22 -0500

    Added script for fields.json

--- scripts/lib/contentful/validate-template.ts
+++ scripts/lib/contentful/validate-template.ts
@@ -1,47 +1,10 @@
-import { ACCESS_TOKEN, CONTENT_TYPE, ENV_PATH } from './constants'
 import type { Template } from './types'
-import initContentful from './index.js'
-
-// List of the fields we'll use to create an entry for the template in Contentful.
-const FIELDS: (keyof Template)[] = [
-  'name',
-  'slug',
-  'description',
-  'framework',
-  'type',
-  'css',
-  'githubUrl',
-  'deployUrl',
-  'demoUrl',
-  'publisher',
-  'relatedTemplates',
-]
-
-type Field = {
-  id: string
-  type: 'Text' | 'Array'
-  required: boolean
-  validations: {
-    size?: { min: number; max: number }
-    regexp?: { pattern: string }
-  }[]
-  items?: {
-    type: 'Symbol' | 'Link'
-    validations: { in?: string[] }[]
-  }
-}
-
-type TemplateFields = ({ id: keyof Template } & Field)[]
+import getTemplateFields from './get-template-fields'
 
 export default async function validateTemplate(
   template: Partial<Template>
 ): Promise<Template> {
-  const contentful = initContentful(ACCESS_TOKEN)
-  // Ref: https://www.contentful.com/developers/docs/references/content-management-api/#/reference/content-types/content-type/get-a-single-content-type/console/curl
-  const contentType = (await contentful(
-    `${ENV_PATH}/content_types/${CONTENT_TYPE}`
-  )) as { fields: TemplateFields }
-  const fields = contentType.fields.filter(({ id }) => FIELDS.includes(id))
+  const fields = await getTemplateFields()
   const errors: Error[] = []
 
   fields.forEach(({ id, type, required, validations, items }) => {
@@ -54,7 +17,7 @@ export default async function validateTemplate(
     // Ignore unrequired values that are empty
     if (!val?.length) return
 
-    if (type === 'Text' && typeof val !== 'string') {
+    if ((type === 'Text' || type === 'Symbol') && typeof val !== 'string') {
       errors.push(new Error(`"${id}" must be a string`))
       return
     }
