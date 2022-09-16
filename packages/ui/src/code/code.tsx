4352ce16d8810a493e65fb5760d6b0a7999a195a
LFades <luis@vercel.com>
2022-09-16 14:12:29 -0500

    Updated code component

--- packages/ui/src/code/code.tsx
+++ packages/ui/src/code/code.tsx
@@ -1,10 +1,16 @@
 import { FC, HTMLAttributes } from 'react'
 import cn from 'clsx'
-import s from './code.module.css'
-import anchorStyles from '../anchor/anchor.module.css'
+// import s from './code.module.css'
+// import anchorStyles from '../anchor/anchor.module.css'
 
 const Code: FC<HTMLAttributes<HTMLElement>> = ({ className, ...props }) => (
-  <code className={cn(s.root, anchorStyles.inlineCode, className)} {...props} />
+  <code
+    className={cn(
+      'text-code text-base whitespace-pre-wrap before:content-["`"] after:content-["`"]',
+      className
+    )}
+    {...props}
+  />
 )
 
 export default Code
