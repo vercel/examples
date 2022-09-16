1df65ee3931cf05985952eef76f6c0c758d9c96d
LFades <luis@vercel.com>
2022-09-15 23:03:58 -0500

    Moved root styles to tailwind

--- packages/ui/src/tailwind.js
+++ packages/ui/src/tailwind.js
@@ -1,24 +1,92 @@
+const colors = {
+  blue: '#2882ef',
+  lightblue: '#2882ef',
+  violet: '#8524c3',
+  rosa: '#f02888',
+  yellow: '#f9c32a',
+  orange: '#f97449',
+  accents: {
+    0: '#fff',
+    1: '#fafafa',
+    2: '#eaeaea',
+    3: '#999',
+    4: '#888',
+    5: '#666',
+    6: '#444',
+    7: '#333',
+    8: '#111',
+    9: '#000',
+  },
+  success: {
+    DEFAULT: '#0070f3',
+    dark: '#0761d1',
+    light: '#3291ff',
+    lighter: '#d3e5ff',
+  },
+  error: {
+    DEFAULT: '#e00',
+    dark: '#c50000',
+    light: '#ff1a1a',
+    lighter: '#f7d4d6',
+  },
+  warning: {
+    DEFAULT: '#f5a623',
+    dark: '#ab570a',
+    light: '#f7b955',
+    lighter: '#ffefcf',
+  },
+  violet: {
+    DEFAULT: '#7928ca',
+    dark: '#4c2889',
+    light: '#8a63d2',
+    lighter: '#e3d7fc',
+  },
+  cyan: {
+    DEFAULT: '#50e3c2',
+    dark: '#29bc9b',
+    light: '#79ffe1',
+    lighter: '#aaffec',
+  },
+  highlight: {
+    purple: '#f81ce5',
+    magenta: '#eb367f',
+    pink: '#ff0080',
+    yellow: '#fff500',
+  },
+  foreground: '#000',
+  background: '#fff',
+}
+
 module.exports = {
   // `content` is replaced instead of extended, so this line has to be added in
   // the `content` of each app' tailwind.config.js
   content: ['node_modules/@vercel/examples-ui/ui/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'system-ui',
+          'BlinkMacSystemFont',
+          'Helvetica Neue',
+          'Helvetica',
+          'sans-serif',
+        ],
+      },
       colors: {
-        'accents-0': 'var(--accents-0)',
-        'accents-1': 'var(--accents-1)',
-        'accents-2': 'var(--accents-2)',
-        'accents-3': 'var(--accents-3)',
-        'accents-4': 'var(--accents-4)',
-        'accents-5': 'var(--accents-5)',
-        'accents-6': 'var(--accents-6)',
-        'accents-7': 'var(--accents-7)',
-        'accents-8': 'var(--accents-8)',
-        'accents-9': 'var(--accents-9)',
-        'success-lighter': 'var(--geist-success-lighter)',
-        'success-light': 'var(--geist-success-light)',
-        success: 'var(--geist-success-lighter)',
-        'success-dark': 'var(--geist-success-dark)',
+        ...colors,
+        selection: cyan.light,
+        link: {
+          DEFAULT: success.DEFAULT,
+          light: success.light,
+        },
+        code: colors.rosa,
+        secondary: {
+          DEFAULT: colors.accents[5],
+          dark: colors.accents[7],
+          light: colors.accents[3],
+          lighter: colors.accents[2],
+        },
       },
     },
   },
