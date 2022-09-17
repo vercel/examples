cf437def7d581f470df8aeb43ba31803586506e9
LFades <luis@vercel.com>
2022-09-17 18:31:02 -0500

    Updated link

--- packages/ui/src/code/code.tsx
+++ packages/ui/src/code/code.tsx
@@ -1,7 +1,5 @@
 import { FC, HTMLAttributes } from 'react'
 import cn from 'clsx'
-// import s from './code.module.css'
-// import anchorStyles from '../anchor/anchor.module.css'
 
 const Code: FC<HTMLAttributes<HTMLElement>> = ({ className, ...props }) => (
   <code
--- packages/ui/src/link/link.tsx
+++ packages/ui/src/link/link.tsx
@@ -1,13 +1,20 @@
 import type { AnchorHTMLAttributes, FC } from 'react'
 import NextLink, { LinkProps } from 'next/link'
 import cn from 'clsx'
-import s from '../anchor/anchor.module.css'
 
 const Link: FC<
   Exclude<LinkProps, 'passHref'> & AnchorHTMLAttributes<HTMLAnchorElement>
 > = ({ href, prefetch, children, className, ...props }) => (
   <NextLink href={href} prefetch={prefetch}>
-    <a className={cn(s.root, className)} {...props}>
+    <a
+      className={cn(
+        'text-link hover:text-link-light transition-colors no-underline',
+        // CSS for <code/>
+        '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
+        className
+      )}
+      {...props}
+    >
       {children}
     </a>
   </NextLink>
