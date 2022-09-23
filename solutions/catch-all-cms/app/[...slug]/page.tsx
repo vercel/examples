45644480bf4e527f53ec97e3287e0a6cc3c4962e
LFades <luis@vercel.com>
2022-09-23 16:05:28 -0500

    Moved content

---
+++ solutions/catch-all-cms/app/[...slug]/page.tsx
@@ -0,0 +1,41 @@
+import { type FC, experimental_use as use } from 'react'
+import cms from 'lib/cms'
+import { RenderCMSComponent } from '../components/index.server'
+
+// export async function getStaticProps({
+//   params,
+// }: {
+//   params: { slug: string[] }
+// }) {
+//   const slug = `/${params.slug.join('/')}`
+//   const page = await cms.getPageBySlug(slug)
+
+//   if (!page) {
+//     return { notFound: true }
+//   }
+
+//   return { props: { page } }
+// }
+
+async function getPageBySlug(slug: string[]) {
+  const page = await cms.getPageBySlug(`/${slug.join('/')}`)
+
+  if (!page) {
+    return { notFound: true }
+  }
+
+  return page
+}
+
+const SlugPage: FC<{ params: { slug: string[] } }> = ({ params: { slug } }) => {
+  const page = use(getPageBySlug(slug))
+
+  console.log('PAGE', page)
+
+  return (
+    // <h1>Hello world</h1>
+    <RenderCMSComponent component={page!} />
+  )
+}
+
+export default SlugPage
