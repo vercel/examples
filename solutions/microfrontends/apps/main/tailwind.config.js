c6c156b1ddd9c8a23fa696a1c31332b28065d1b9
LFades <luis@vercel.com>
2022-10-24 21:33:47 -0500

    Updated design system

--- solutions/microfrontends/apps/main/tailwind.config.js
+++ solutions/microfrontends/apps/main/tailwind.config.js
@@ -1,9 +1,14 @@
-const { getContent } = require('@acme/tailwind-config/content')
-
 module.exports = {
-  presets: [require('@acme/tailwind-config')],
-  content: getContent([
+  presets: [
+    require('@vercel/examples-ui/tailwind'),
+    require('@acme/design-system/tailwind'),
+  ],
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-  ]),
+    // Add the external packages that are using Tailwind CSS
+    './node_modules/@vercel/examples-ui/**/*.js',
+    './node_modules/@acme/design-system/**/*.js',
+    './node_modules/@acme/pages/**/*.js',
+  ],
 }
