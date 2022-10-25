bbcce4c12c8d223fb1e8784c5d187eca9c7fde7e
LFades <luis@vercel.com>
2022-10-25 01:08:35 -0500

    Updated Link component in @vercel/examples-ui

--- packages/ui/src/link.tsx
+++ packages/ui/src/link.tsx
@@ -1,23 +1,35 @@
-import type { AnchorHTMLAttributes, FC } from 'react'
+import type { FC } from 'react'
 import NextLink, { LinkProps } from 'next/link.js'
 import cn from 'clsx'
 
-const Link: FC<
-  Exclude<LinkProps, 'passHref'> & AnchorHTMLAttributes<HTMLAnchorElement>
-> = ({ href, prefetch, children, className, ...props }) => (
-  <NextLink href={href} prefetch={prefetch}>
-    <a
-      className={cn(
-        'text-link hover:text-link-light transition-colors no-underline',
-        // CSS for <code/>
-        '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </a>
+// TODO: replace this with `LinkProps` once it's including the anchor props
+type LinkPropsReal = React.PropsWithChildren<
+  Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
+    LinkProps
+>
+
+const css = [
+  'text-link hover:text-link-light transition-colors no-underline',
+  // CSS for <code/>
+  '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
+]
+
+const Link: FC<LinkPropsReal> = ({ children, className, ...props }) => (
+  <NextLink className={cn(css, className)} {...props}>
+    {children}
   </NextLink>
+)
+
+// A normal anchor tag is also exported for relative links to paths that don't exist in the app.
+// For example apps that are using Multi Zones.
+export const A: FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <a className={cn(css, className)} {...props}>
+    {children}
+  </a>
 )
 
 export default Link
