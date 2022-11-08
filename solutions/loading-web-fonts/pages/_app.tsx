a64e031a2fd149a0b0c052c87bd82ad5b329595c
LFades <luis@vercel.com>
2022-11-07 23:38:14 -0500

    Added a subset

--- solutions/loading-web-fonts/pages/_app.tsx
+++ solutions/loading-web-fonts/pages/_app.tsx
@@ -23,6 +23,9 @@ import '../globals.css'
 const inter = Inter({
   variable: '--inter-font',
   display: 'swap',
+  // subsets to preload, more on:
+  // https://nextjs.org/docs/basic-features/font-optimization#specifying-a-subset
+  subsets: ['latin'],
 })
 
 function App({ Component, pageProps }: AppProps) {
