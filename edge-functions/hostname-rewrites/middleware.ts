1d5d57d87f625484a56e8e2ff1ba6f05f4ed65fb
Sanskar Tiwari <theindianappguy@gmail.com>
2022-11-23 22:12:51 +0530

    Update middleware.ts
    without this / all the subdomain were showing the same data

--- edge-functions/hostname-rewrites/middleware.ts
+++ edge-functions/hostname-rewrites/middleware.ts
@@ -26,7 +26,7 @@ export default async function middleware(req: NextRequest) {
   } else {
     console.log('URL 2', req.nextUrl.href)
     // rewrite to the current subdomain under the pages/sites folder
-    url.pathname = `/_sites/${data.subdomain}${url.pathname}`
+    url.pathname = `/_sites/${data.subdomain}/${url.pathname}`
   }
 
   return NextResponse.rewrite(url)
