5fe9060b5a35eee985f97347fe3f87afc8446f6c
Dominik Ferber <dominik.ferber@gmail.com>
2022-12-15 14:26:51 +0200

    ab-testing-statsig: relax uuid regex (#483)
    ### Description
    
    Relaxes the regex to accept multiple uid formats.

--- edge-functions/ab-testing-statsig/middleware.ts
+++ edge-functions/ab-testing-statsig/middleware.ts
@@ -4,7 +4,7 @@ import { EdgeConfigDataAdapter } from 'statsig-node-vercel'
 import { EXPERIMENT, UID_COOKIE, GROUP_PARAM_FALLBACK } from './lib/constants'
 
 // We'll use this to validate a random UUID
-const IS_UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{16}$/i
+const IS_UUID = /^[0-9a-f-]+$/i
 const dataAdapter = new EdgeConfigDataAdapter(process.env.EDGE_CONFIG_ITEM_KEY!)
 
 export const config = {
@@ -13,12 +13,7 @@ export const config = {
 
 export async function middleware(req: NextRequest) {
   // Get the user ID from the cookie or get a new one
-  let uidCookieValue = req.cookies.get(UID_COOKIE)
-  // In Next.js 13, the value returned from cookies.get() is an object with the
-  // type: { name: string, value: string }
-  // This check makes it compatible with Next.js 12 and 13
-  let userId =
-    typeof uidCookieValue === 'string' ? uidCookieValue : uidCookieValue?.value
+  let userId = req.cookies.get(UID_COOKIE)?.value
   let hasUserId = !!userId
 
   // If there's no active user ID in cookies or its value is invalid, get a new one
