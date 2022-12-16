9939a27bbd4167fdda45e01afb8b43ff2dd740b2
Dominik Ferber <dominik.ferber@gmail.com>
2022-12-16 21:17:15 +0200

    maintenance-page: handle missing EDGE_CONFIG env var

--- edge-functions/maintenance-page/middleware.ts
+++ edge-functions/maintenance-page/middleware.ts
@@ -6,14 +6,20 @@ export const config = {
 }
 
 export async function middleware(req: NextRequest) {
-  // Check whether the maintenance page should be shown
-  const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
+  try {
+    // Check whether the maintenance page should be shown
+    const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
 
-  // If is in maintenance mode, point the url pathname to the maintenance page
-  if (isInMaintenanceMode) {
-    req.nextUrl.pathname = `/maintenance`
+    // If is in maintenance mode, point the url pathname to the maintenance page
+    if (isInMaintenanceMode) {
+      req.nextUrl.pathname = `/maintenance`
 
-    // Rewrite to the url
-    return NextResponse.rewrite(req.nextUrl)
+      // Rewrite to the url
+      return NextResponse.rewrite(req.nextUrl)
+    }
+  } catch (error) {
+    // show the default page if EDGE_CONFIG env var is missing,
+    // but log the error to the console
+    console.error(error)
   }
 }
