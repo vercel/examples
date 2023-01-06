111fa564934c116de897e5ed1f0849c9ec34f21e
Gonzalo Pozzo <gonzalo.pozzo4@gmail.com>
2023-01-06 11:49:08 -0800

    Update json response example (#510)
    Update json response example

--- edge-functions/json-response/middleware.ts
+++ edge-functions/json-response/middleware.ts
@@ -1,11 +1,12 @@
-import { NextResponse, NextRequest } from 'next/server'
-
 export const config = {
   matcher: '/',
 }
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl
-  url.pathname = '/api/json'
-  return NextResponse.rewrite(url)
+export function middleware() {
+  return new Response(JSON.stringify({ message: 'hello world!' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 }
