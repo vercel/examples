24e2d9b64184a5d1869fceee5dd41bdda44c809f
goncy <gonzalo.pozzo4@gmail.com>
2023-01-06 16:41:55 -0300

    Add empty page

--- edge-functions/json-response/middleware.ts
+++ edge-functions/json-response/middleware.ts
@@ -1,10 +1,8 @@
-import { NextRequest } from 'next/server'
-
 export const config = {
   matcher: '/',
 }
 
-export function middleware(req: NextRequest) {
+export function middleware() {
   return new Response(JSON.stringify({ message: 'hello world!' }), {
     status: 200,
     headers: {
---
+++ edge-functions/json-response/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react"
+
+const IndexPage: FC = () => {
+  return (
+    <div>This page should never be reached</div>
+  );
+}
+
+export default IndexPage;
