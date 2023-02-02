434fcddda4ab33f2932a136d13178a26dda41d6e
Michael Novotny <manovotny@gmail.com>
2023-02-02 12:04:17 -0600

    Adds `force-update-templates` npm script.

---
+++ scripts/force-update-templates.ts
@@ -0,0 +1,17 @@
+import { globby } from 'globby'
+import updateChangedTemplates from './lib/update-changed-templates'
+
+const templates = await globby([
+  'edge-api-routes/**/package.json',
+  'edge-functions/**/package.json',
+  'edge-middleware/**/package.json',
+  'starter/**/package.json',
+])
+const changedFiles = templates.map((path) =>
+  path.replace('package.json', 'README.md')
+)
+
+updateChangedTemplates(changedFiles).catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
