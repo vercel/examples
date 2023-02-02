3347a2f7f00e2941e85879d1552a68a53eac6a3a
Balázs Orbán <info@balazsorban.com>
2023-02-02 17:51:07 +0100

    fix: upgrade `next-auth` to fix TS issue (#546)

--- solutions/subdomain-auth/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
