8727e7e2c3f4df23e3125bf2e5e9ad0ed81945e1
Joost <joost@meijles.com>
2023-02-13 21:21:46 +0100

    Fix: Tailwind content path should match TS (#556)
    ### Description
    
    Tailwind configuration is not working because the package content path
    does not on Typescript extensions.
    
    ### Type of Change
    - [x] Example updates (Bug fixes, new features, etc.)

--- solutions/monorepo/packages/ui/src/tailwind.cjs
+++ solutions/monorepo/packages/ui/src/tailwind.cjs
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   // `content` is replaced instead of extended, so this line has to be added in
   // the `content` of each app' tailwind.config.js
-  content: [path.join(path.dirname(require.resolve('@acme/ui')), '**/*.js')],
+  content: [path.join(path.dirname(require.resolve('@acme/ui')), '**/*.{js,ts,jsx,tsx}')],
   theme: {
     extend: {},
   },
