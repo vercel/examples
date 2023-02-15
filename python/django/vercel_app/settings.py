d56091583b249e5ad284e925e4271970854aeb5a
Ali Solanki <55312000+alisolanki@users.noreply.github.com>
2023-02-15 17:40:34 +0530

    Update settings.py for Static Files

--- python/django/vercel_app/settings.py
+++ python/django/vercel_app/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
