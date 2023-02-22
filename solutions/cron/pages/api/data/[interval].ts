e71572eccc59ec645714c871fdab733d8566bc08
Steven Tey <stevensteel97@gmail.com>
2023-02-21 18:44:23 -0600

    added failsafe

--- solutions/cron/pages/api/data/[interval].ts
+++ solutions/cron/pages/api/data/[interval].ts
@@ -8,7 +8,7 @@ export const config = {
 export default async function handler(req: NextRequest) {
   const interval = req.nextUrl.searchParams.get('interval')
   if (!interval) return new Response('No interval provided', { status: 400 })
-  const { id, fetchedAt } = await get(interval)
+  const { id, fetchedAt } = (await get(interval)) || {}
   const res = await fetch(
     `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
   ).then((res) => res.json())
