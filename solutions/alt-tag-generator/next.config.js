b67b1d7fdb20c3a569710ccea1a5a48671b3de8b
Steven Tey <stevensteel97@gmail.com>
2023-03-07 10:56:26 -0600

    Add redirects to GH Repo & Vercel Templates Page (#584)
    ### Description
    
    <!--
    ✍️ Write a short summary of your work. Screenshots and videos are
    welcome!
    -->
    
    ### Demo URL
    
    <!--
    Provide a URL to a live deployment where we can test your PR. If a demo
    isn't possible feel free to omit this section.
    -->
    
    ### Type of Change
    
    - [ ] New Example
    - [ ] Example updates (Bug fixes, new features, etc.)
    - [ ] Other (changes to the codebase, but not to examples)
    
    ### New Example Checklist
    
    - [ ] 🛫 `npm run new-example` was used to create the example
    - [ ] 📚 The template wasn't used but I carefuly read the [Adding a new
    example](https://github.com/vercel/examples#adding-a-new-example) steps
    and implemented them in the example
    - [ ] 📱 Is it responsive? Are mobile and tablets considered?

--- solutions/alt-tag-generator/next.config.js
+++ solutions/alt-tag-generator/next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/vercel/examples/tree/main/solutions/alt-tag-generator",
+        permanent: false,
+      },
+      {
+        source: "/deploy",
+        destination: "https://vercel.com/templates/next.js/ai-alt-text-generator",
+        permanent: false,
+      },   
+    ]
+  },
 }
 
 module.exports = nextConfig
