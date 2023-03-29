43beb52213cb160c7366f12cdce1b9a51e47fa83
goncy <gonzalo.pozzo4@gmail.com>
2023-03-29 16:05:36 -0300

    Fix country example

--- edge-middleware/geolocation-country-block/pages/[country].tsx
+++ edge-middleware/geolocation-country-block/pages/[country].tsx
@@ -1,10 +1,32 @@
-import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+import { ParsedUrlQuery } from 'querystring'
 
-const AllowedPage: React.FC = () => {
-  const {
-    query: { country },
-  } = useRouter()
+interface Props {
+  country: string
+}
 
+interface Params extends ParsedUrlQuery {
+  country: string
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params: { country },
+}) => {
+  return {
+    props: {
+      country,
+    },
+  }
+}
+
+const AllowedPage = ({ country }: Props) => {
   return <p>{`Greetings from ${country}, where you are not blocked.`}</p>
 }
 
