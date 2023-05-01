52b0d3cd1bb02b10de10cd76ee408db5aa6abdbb
Drew Radcliff <sirampr@gmail.com>
2023-05-01 15:20:23 -0400

    Fix comment in kysely.ts

--- storage/postgres-kysely/lib/kysely.ts
+++ storage/postgres-kysely/lib/kysely.ts
@@ -12,7 +12,7 @@ interface UserTable {
 
   // You can specify a different type for each operation (select, insert and
   // update) using the `ColumnType<SelectType, InsertType, UpdateType>`
-  // wrapper. Here we define a column `modified_at` that is selected as
+  // wrapper. Here we define a column `createdAt` that is selected as
   // a `Date`, can optionally be provided as a `string` in inserts and
   // can never be updated:
   createdAt: ColumnType<Date, string | undefined, never>;
