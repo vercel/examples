cc3fbf2ebbd294f1773cd5e86f98908cadd7102b
Steven Tey <stevensteel97@gmail.com>
2023-05-01 09:54:03 -0500

    Added Storage Templates

---
+++ storage/postgres-kysely/lib/utils.ts
@@ -0,0 +1,8 @@
+import ms from 'ms'
+
+export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
+  if (!timestamp) return 'never'
+  return `${ms(Date.now() - new Date(timestamp).getTime())}${
+    timeOnly ? '' : ' ago'
+  }`
+}
