d7af2e44f18ccd40a7e42ef8dce6b030c72286d6
Jared Palmer <jared@jaredpalmer.com>
2023-05-01 11:14:24 -0700

    Fix deploy button

--- storage/postgres-pgvector/app/page.tsx
+++ storage/postgres-pgvector/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <Link
-        href="https://vercel.com"
+        href="https://vercel.com/templates/next.js/postgres-pgvector"
         className="group rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
         <p>Deploy your own to Vercel</p>
