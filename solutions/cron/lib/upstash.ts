f49a17e9e060e9741f361992262ecce75a927c3f
Steven Tey <stevensteel97@gmail.com>
2023-05-03 14:24:05 -0500

    Update cron example to use Vercel KV (#683)

--- solutions/cron/lib/upstash.ts
+++
@@ -1,6 +0,0 @@
-import { Redis } from '@upstash/redis'
-
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL || '',
-  token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
-})
