0c15bc9c755914e3782083acef3b239b28aff339
Sashank Thupukari <s@thupukari.com>
2023-05-04 16:43:25 -0400

    Update layout.tsx to reference Kysely instead of Prisma

--- storage/postgres-kysely/app/layout.tsx
+++ storage/postgres-kysely/app/layout.tsx
@@ -2,9 +2,9 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  title: 'Vercel Postgres Demo with Prisma',
+  title: 'Vercel Postgres Demo with Kysely',
   description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
+    'A simple Next.js app with Vercel Postgres as the database and Kysely as the ORM',
 }
 
 const inter = Inter({
