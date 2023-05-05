105f7f8ebec96939fba2f9d1eb0d5a5380650bd8
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-05-05 11:02:17 +0200

    update template

--- storage/blob-sveltekit/src/routes/+page.server.ts
+++ storage/blob-sveltekit/src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import { put } from '@vercel/blob'
 export const actions = {
   upload: async ({ request }) => {
     const form = await request.formData()
-    const file = form.get('file') as File
+    const file = form.get('image-upload') as File
 
     if (!file) {
       throw error(400, { message: 'No file to upload.' })
--- storage/blob-sveltekit/vite.config.js
+++ storage/blob-sveltekit/vite.config.js
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import dotenvExpand from 'dotenv-expand'
+import { loadEnv, defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [sveltekit()],
+export default defineConfig(({ mode }) => {
+  // This check is important!
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '')
+    dotenvExpand.expand({ parsed: env })
+  }
+  return {
+    plugins: [sveltekit()],
+  }
 })
