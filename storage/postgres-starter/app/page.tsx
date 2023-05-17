82f7248bf430c65a91f2012d3a129133dbad1392
Steven Tey <stevensteel97@gmail.com>
2023-05-17 13:13:05 -0500

    Update to use edge runtime

--- storage/postgres-starter/app/page.tsx
+++ storage/postgres-starter/app/page.tsx
@@ -5,7 +5,7 @@ import Table from '@/components/table'
 import TablePlaceholder from '@/components/table-placeholder'
 import ExpandingArrow from '@/components/expanding-arrow'
 
-// export const runtime = 'edge'
+export const runtime = 'edge'
 export const preferredRegion = 'home'
 export const dynamic = 'force-dynamic'
 
