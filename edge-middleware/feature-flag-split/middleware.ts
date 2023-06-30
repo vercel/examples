a19211aa0fb1e0d66152ea8456d5a7760eee2e74
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-06-30 13:36:08 -0300

    update dependencies

--- edge-middleware/feature-flag-split/middleware.ts
+++ edge-middleware/feature-flag-split/middleware.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { SplitFactory, PluggableStorage, ErrorLogger } from '@splitsoftware/splitio-browserjs';
-import { EdgeConfigWrapper } from '@splitsoftware/vercel-integration-utils';
-import * as EdgeConfigClient from '@vercel/edge-config';
+import {
+  SplitFactory,
+  PluggableStorage,
+  ErrorLogger,
+} from '@splitsoftware/splitio-browserjs'
+import { EdgeConfigWrapper } from '@splitsoftware/vercel-integration-utils'
+import * as EdgeConfigClient from '@vercel/edge-config'
 
 export const config = {
   matcher: ['/about', '/marketing'],
@@ -19,37 +23,37 @@ export async function middleware(req: NextRequest) {
   const client = SplitFactory({
     core: {
       authorizationKey: process.env.SPLIT_SDK_KEY,
-      key: userKey
+      key: userKey,
     },
     mode: 'consumer_partial',
     storage: PluggableStorage({
       wrapper: EdgeConfigWrapper({
         // The Edge Config item where Split stores feature flag definitions, specified in the Split integration step
-        edgeConfigKey: process.env.SPLIT_EDGE_CONFIG_ITEM_KEY,
+        edgeConfigItemKey: process.env.SPLIT_EDGE_CONFIG_ITEM_KEY,
         // The Edge Config client. In this case, we are passing the default client
         // that reads from the Edge Config stored in process.env.EDGE_CONFIG
-        edgeConfig: EdgeConfigClient
-      })
+        edgeConfig: EdgeConfigClient,
+      }),
     }),
     // Disable or keep only ERROR log level in production, to minimize performance impact
     debug: ErrorLogger(),
-  }).client();
+  }).client()
 
   // Wait until the SDK is ready or timed out. If timeout occurs, treatment evaluations will default to 'control'.
   // A timeout should not occur if Edge Config is properly configured and synchronized.
-  await new Promise(res => {
-    client.on(client.Event.SDK_READY, res);
-    client.on(client.Event.SDK_READY_TIMED_OUT, res);
-  });
+  await new Promise((res) => {
+    client.on(client.Event.SDK_READY, res)
+    client.on(client.Event.SDK_READY_TIMED_OUT, res)
+  })
 
   // Evaluate a feature flag. In this case, we use the result treatment to overwrite the pathname
-  const treatment = await client.getTreatment(FEATURE_FLAG_NAME);
-  if (treatment === 'on') req.nextUrl.pathname = `${req.nextUrl.pathname}/b`;
+  const treatment = await client.getTreatment(FEATURE_FLAG_NAME)
+  if (treatment === 'on') req.nextUrl.pathname = `${req.nextUrl.pathname}/b`
 
   const res = NextResponse.rewrite(req.nextUrl)
 
   // Add the cookie if it's not there
-  if (!req.cookies.has(SPLIT_COOKIE)) res.cookies.set(SPLIT_COOKIE, userKey);
+  if (!req.cookies.has(SPLIT_COOKIE)) res.cookies.set(SPLIT_COOKIE, userKey)
 
   return res
 }
