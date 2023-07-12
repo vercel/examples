e15c0d10f93e372bb0aa0945ed1725a2ef9ba71c
Dominik Ferber <dominik.ferber@gmail.com>
2023-07-12 09:51:11 +0300

    upgrade to latest version of @splitsoftware/vercel-integration-utils

--- edge-middleware/feature-flag-split/lib/split.ts
+++ edge-middleware/feature-flag-split/lib/split.ts
@@ -4,6 +4,9 @@ import {
   ErrorLogger,
 } from '@splitsoftware/splitio-browserjs'
 import { EdgeConfigWrapper } from '@splitsoftware/vercel-integration-utils'
+import { createClient } from '@vercel/edge-config'
+
+const edgeConfigClient = createClient(process.env.EDGE_CONFIG)
 
 export async function createSplitClient(userKey: string) {
   const client = SplitFactory({
@@ -15,7 +18,8 @@ export async function createSplitClient(userKey: string) {
     storage: PluggableStorage({
       wrapper: EdgeConfigWrapper({
         // The Edge Config item where Split stores feature flag definitions, specified in the Split integration step
-        edgeConfigKey: process.env.EDGE_CONFIG_SPLIT_ITEM_KEY!,
+        edgeConfigItemKey: process.env.EDGE_CONFIG_SPLIT_ITEM_KEY!,
+        edgeConfig: edgeConfigClient,
       }),
     }),
     // Disable or keep only ERROR log level in production, to minimize performance impact
