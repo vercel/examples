7601a6f2cafdd5ad24b08b891c51bed2b119a152
Dominik Ferber <dominik.ferber@gmail.com>
2023-07-12 16:11:58 +0300

    extract icon

--- edge-middleware/feature-flag-split/app/missing-edge-config/page.tsx
+++ edge-middleware/feature-flag-split/app/missing-edge-config/page.tsx
@@ -1,3 +1,24 @@
+/* Heroicon name: outline/exclamation-triangle */
+function ExclamantionTriangleIcon() {
+  return (
+    <svg
+      className="h-6 w-6 text-gray-600"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z"
+      />
+    </svg>
+  )
+}
+
 export default function MissingEdgeConfigDialog() {
   return (
     <div
@@ -12,22 +33,7 @@ export default function MissingEdgeConfigDialog() {
           <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
             <div className="sm:flex sm:items-start">
               <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
-                {/* Heroicon name: outline/exclamation-triangle */}
-                <svg
-                  className="h-6 w-6 text-gray-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z"
-                  />
-                </svg>
+                <ExclamantionTriangleIcon />
               </div>
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h3
