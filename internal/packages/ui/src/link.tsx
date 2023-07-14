28985a5b3b83de3fd453060f9d94dcb578c84b71
Luis Alvarez <luis@vercel.com>
2023-07-14 12:46:40 -0500

    Updated Link

--- internal/packages/ui/src/link.tsx
+++ internal/packages/ui/src/link.tsx
@@ -6,41 +6,49 @@ type LinkProps = Omit<
   keyof NextLinkProps
 > &
   NextLinkProps & {
+    component?: 'a' | 'Link'
     secondary?: boolean
   }
-
-const css = 'transition-colors no-underline'
-const primaryCss = [
-  'text-link hover:text-link-light',
-  // CSS for <code/>
-  '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
-]
-const secondaryCss = 'text-accents-6 duration-200 hover:text-accents-8'
 
 export const Link = ({
   children,
   className,
   href,
+  component,
   secondary,
   ...props
-}: LinkProps) => (
-  <NextLink
-    href={href}
-    className={clsx(css, secondary ? secondaryCss : primaryCss, className)}
-    {...props}
-  >
-    {children}
-  </NextLink>
-)
+}: LinkProps) => {
+  // A normal anchor tag is also supported for relative links to paths that don't exist in the app.
+  // For example apps that are using Multi Zones.
+  const Component: string | typeof NextLink = component === 'a' ? 'a' : NextLink
+  return (
+    <Component
+      href={href}
+      className={clsx(
+        'transition-colors no-underline',
+        secondary
+          ? 'text-accents-6 duration-200 hover:text-accents-8'
+          : [
+              'text-link hover:text-link-light',
+              // CSS for <code/>
+              '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
+            ],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  )
+}
 
-// A normal anchor tag is also exported for relative links to paths that don't exist in the app.
-// For example apps that are using Multi Zones.
 export const A = ({
+  href,
   children,
   className,
   ...props
 }: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
-  <a className={clsx(css, className)} {...props}>
+  <Link href={href!} component="a" {...props}>
     {children}
-  </a>
+  </Link>
 )
