d0aec636d75909dcac34918f2952bbd9ca94827b
Luis Alvarez <luis@vercel.com>
2023-07-14 17:32:19 -0500

    Moved Page component

--- internal/packages/ui/src/index.tsx
+++ internal/packages/ui/src/index.tsx
@@ -1,12 +1,10 @@
-import type { FC, HTMLAttributes } from 'react'
-import cn from 'clsx'
-
 export * from './layout.js'
 export * from './head.js'
 export * from './text.js'
 export * from './input.js'
 export * from './button.js'
 export * from './loading-dots.js'
+export * from './page.js'
 export * from './link.js'
 export * from './code.js'
 export * from './list.js'
@@ -22,13 +20,3 @@ export const getMetadata = ({
   title: title ? `${title} - Vercel Examples` : 'Vercel Example',
   description,
 })
-
-export const Page: FC<HTMLAttributes<HTMLElement>> = ({
-  children,
-  className,
-  ...props
-}) => (
-  <main {...props} className={cn('w-full max-w-3xl mx-auto py-16', className)}>
-    {children}
-  </main>
-)
