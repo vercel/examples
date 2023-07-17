285ad9eeeb45c93f6618be2463ffd3b1e1ace520
Luis Alvarez D <luis@vercel.com>
2023-07-17 12:08:50 -0500

    Fix examples UI package issues with the app router (#743)
    This PR only includes changes to the `@vercel/examples-ui` package
    itself and a test app where all the components from it are used in both
    pages and the App Router. Once this is merged and the new version
    released I'll update the examples that are currently applying some
    workarounds to get the package working.
    
    ### Type of Change
    
    - [ ] New Example
    - [ ] Example updates (Bug fixes, new features, etc.)
    - [x] Other (changes to the codebase, but not to examples)

--- internal/packages/ui/src/link.tsx
+++ internal/packages/ui/src/link.tsx
@@ -1,37 +1,54 @@
-import type { FC } from 'react'
-import NextLink, { LinkProps } from 'next/link.js'
-import cn from 'clsx'
+import NextLink, { LinkProps as NextLinkProps } from 'next/link.js'
+import clsx from 'clsx'
 
-// TODO: replace this with `LinkProps` once it's including the anchor props
-type LinkPropsReal = React.PropsWithChildren<
-  Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
-    LinkProps
->
+type LinkProps = Omit<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  keyof NextLinkProps
+> &
+  NextLinkProps & {
+    component?: 'a' | 'Link'
+    secondary?: boolean
+  }
 
-const css = [
-  'text-link hover:text-link-light transition-colors no-underline',
-  // CSS for <code/>
-  '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
-]
-
-const Link: FC<LinkPropsReal> = ({ children, className, href, ...props }) => (
-  <NextLink href={href} legacyBehavior>
-    <a className={cn(css, className)} {...props}>
+export const Link = ({
+  children,
+  className,
+  href,
+  component,
+  secondary,
+  ...props
+}: LinkProps) => {
+  // A normal anchor tag is also supported for relative links to paths that don't exist in the app.
+  // For example apps that are using Multi Zones.
+  const Component: string | typeof NextLink = component === 'a' ? 'a' : NextLink
+  return (
+    <Component
+      href={href}
+      className={clsx(
+        'transition-colors no-underline',
+        secondary
+          ? 'text-accents-6 duration-200 hover:text-accents-8'
+          : [
+              'text-link hover:text-link-light',
+              // CSS for <code/>
+              '[&_code]:text-link [&_code]:hover:text-link-light [&_code]:transition-colors',
+            ],
+        className
+      )}
+      {...props}
+    >
       {children}
-    </a>
-  </NextLink>
-)
+    </Component>
+  )
+}
 
-// A normal anchor tag is also exported for relative links to paths that don't exist in the app.
-// For example apps that are using Multi Zones.
-export const A: FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = ({
+export const A = ({
+  href,
   children,
   className,
   ...props
-}) => (
-  <a className={cn(css, className)} {...props}>
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
+  <Link href={href!} component="a" {...props}>
     {children}
-  </a>
+  </Link>
 )
-
-export default Link
--- internal/packages/ui/src/snippet.tsx
+++ internal/packages/ui/src/snippet.tsx
@@ -1,27 +1,18 @@
-import { FC, HTMLAttributes, useMemo } from 'react'
-import cn from 'clsx'
-import { highlight } from 'sugar-high'
+import { type HTMLAttributes } from 'react'
+import clsx from 'clsx'
 
-const Snippet: FC<HTMLAttributes<HTMLPreElement>> = ({
+export const Snippet = ({
   children,
   className,
   ...props
-}) => {
-  const __html = useMemo(
-    () => (children ? highlight(children.toString()!) : ''),
-    [children]
-  )
-  return (
-    <pre
-      className={cn(
-        'border border-accents-2 rounded-md bg-accents-1 overflow-x-auto p-6',
-        className
-      )}
-      {...props}
-    >
-      <code dangerouslySetInnerHTML={{ __html }} />
-    </pre>
-  )
-}
-
-export default Snippet
+}: HTMLAttributes<HTMLPreElement>) => (
+  <pre
+    className={clsx(
+      'border border-accents-2 rounded-md bg-accents-1 overflow-x-auto p-6',
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </pre>
+)
