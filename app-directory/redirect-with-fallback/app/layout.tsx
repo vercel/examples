76ab68ca23776002b71d1fe600c1c12207751d00
Luis Alvarez <luis@vercel.com>
2023-07-17 23:39:09 -0500

    Updated redirect-with-fallback

--- app-directory/redirect-with-fallback/app/layout.tsx
+++ app-directory/redirect-with-fallback/app/layout.tsx
@@ -1,38 +1,17 @@
-import Nav from '@vercel/examples-ui/nav'
-import Vercel from '@vercel/examples-ui/icons/vercel'
-
+import type { ReactNode } from 'react'
+import { Layout, getMetadata } from '@vercel/examples-ui'
 import '@vercel/examples-ui/globals.css'
 
-interface Props {
-  children: React.ReactNode
-}
+export const metadata = getMetadata({
+  title: 'Redirects with fallback',
+  description: 'Learn how to use a Next.js route handler for redirects',
+})
 
-export default async function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html>
-      <head />
+    <html lang="en">
       <body>
-        <div className="mx-auto h-screen flex flex-col">
-          <Nav path="app-directory/redirect-with-fallback" />
-          <div className="px-8 bg-accents-0">
-            <main>{children}</main>
-            <footer className="py-10 w-full mt-auto border-t flex items-center justify-center bg-accents-1 z-20">
-              <span className="text-primary">Created by</span>
-              <a
-                href="https://vercel.com"
-                aria-label="Vercel.com Link"
-                target="_blank"
-                rel="noreferrer"
-                className="text-black"
-              >
-                <Vercel
-                  className="inline-block h-6 ml-3 text-primary"
-                  alt="Vercel.com Logo"
-                />
-              </a>
-            </footer>
-          </div>
-        </div>
+        <Layout path="app-directory/css-in-js">{children}</Layout>
       </body>
     </html>
   )
