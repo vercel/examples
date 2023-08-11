a2c4e7bedf4a336e4cfd51d6d79724820ee246a7
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-08-11 15:47:18 +0200

    [storage] update nextjs, remove experimental app (#763)

--- storage/postgres-drizzle/next.config.js
+++ storage/postgres-drizzle/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ["pbs.twimg.com"],
+    domains: ['pbs.twimg.com'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
