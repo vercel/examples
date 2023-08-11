0826dc09683dc89323aeed1bf2b78cbe3766cd69
Miraan Tabrez <miraan.tabrez@gmail.com>
2023-08-11 11:53:27 +0100

    add post-deployment instructions to readme

--- edge-middleware/feature-flag-hypertune/app/page.tsx
+++ edge-middleware/feature-flag-hypertune/app/page.tsx
@@ -17,10 +17,65 @@ async function getData() {
 export default async function Home() {
   const data = await getData()
   return (
-    <div>
-      <h1>Hypertune with Vercel Edge Config</h1>
-      <div>Server-side feature flag: {String(data.exampleFlag)}</div>
-      <ClientExample />
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <div style={{ maxWidth: 700, marginTop: 25 }}>
+        <h1>Hypertune with Vercel Edge Config</h1>
+        <div>
+          Server-side feature flag: <strong>{String(data.exampleFlag)}</strong>
+        </div>
+        <ClientExample />
+        <p>
+          Once you've deployed this project, open the{' '}
+          <a href="https://app.hypertune.com/" target="_blank">
+            Hypertune console
+          </a>{' '}
+          and try updating your feature flag logic.
+        </p>
+        <p>To develop your project locally:</p>
+        <ol>
+          <li>
+            Clone your project's repository and <strong>cd</strong> into it
+          </li>
+          <li>
+            Run <strong>vercel link</strong> to link to the Vercel project
+          </li>
+          <li>
+            Run <strong>vercel env pull .env.development.local</strong> to pull
+            your environment variables
+          </li>
+          <li>
+            Copy the <strong>NEXT_PUBLIC_HYPERTUNE_TOKEN</strong> variable from
+            <strong>.env.development.local</strong> to <strong>.env</strong>
+          </li>
+          <li>
+            Run <strong>npm i</strong>
+          </li>
+          <li>
+            Run <strong>npm run dev</strong>
+          </li>
+        </ol>
+        <p>
+          This example assumes your Hypertune project has an{' '}
+          <strong>exampleFlag</strong> feature flag defined on the{' '}
+          <strong>root</strong> field in your project schema. If you created a
+          new Hypertune project while installing the integration, it will have
+          this feature flag by default. But if you connected an existing
+          Hypertune project without this example flag, follow the instructions
+          below:
+        </p>
+        <p>
+          To add new feature flags, define them in your project schema and
+          configure their logic in the{' '}
+          <a href="https://app.hypertune.com/" target="_blank">
+            Hypertune console
+          </a>
+          . Then add them to <strong>hypertune.graphql</strong> and run{' '}
+          <strong>npx hypertune</strong> to generate type-safe methods for them
+          which you can use in your app.
+        </p>
+      </div>
     </div>
   )
 }
--- edge-middleware/feature-flag-hypertune/lib/ClientExample.tsx
+++ edge-middleware/feature-flag-hypertune/lib/ClientExample.tsx
@@ -6,5 +6,9 @@ export default function ClientExample() {
   const rootNode = useHypertune()
   const exampleFlag = rootNode.exampleFlag({}).get(false)
   console.log('Client-side feature flag:', exampleFlag)
-  return <div>Client-side feature flag: {String(exampleFlag)}</div>
+  return (
+    <div>
+      Client-side feature flag: <strong>{String(exampleFlag)}</strong>
+    </div>
+  )
 }
