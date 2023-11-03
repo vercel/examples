5dd92f4b097adedb2a248864af37efeb3b8496b4
Michael Novotny <manovotny@gmail.com>
2023-11-03 13:46:56 -0500

    Fixes postgres-starter template (#818)

--- storage/postgres-starter/next.config.js
+++ storage/postgres-starter/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
+const nextConfig = {
   images: {
-    domains: ['pbs.twimg.com'],
+    domains: ["images.ctfassets.net"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
