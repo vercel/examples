8c79eb9c720cfa59a47a11d103c28b9ab1440b69
Luis Meyer <luis.meyer@vercel.com>
2023-11-15 16:03:49 +0100

    Update image urls in all postgres examples

--- storage/postgres-sveltekit/src/routes/+page.server.js
+++ storage/postgres-sveltekit/src/routes/+page.server.js
@@ -17,17 +17,17 @@ async function seed() {
   const users = await Promise.all([
     sql`
           INSERT INTO users (name, email, image)
-          VALUES ('Guillermo Rauch', 'rauchg@vercel.com', 'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg')
+          VALUES ('Guillermo Rauch', 'rauchg@vercel.com', 'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg')
           ON CONFLICT (email) DO NOTHING;
       `,
     sql`
           INSERT INTO users (name, email, image)
-          VALUES ('Lee Robinson', 'lee@vercel.com', 'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg')
+          VALUES ('Lee Robinson', 'lee@vercel.com', 'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg')
           ON CONFLICT (email) DO NOTHING;
       `,
     sql`
           INSERT INTO users (name, email, image)
-          VALUES ('Steven Tey', 'stey@vercel.com', 'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg')
+          VALUES ('Steven Tey', 'stey@vercel.com', 'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg')
           ON CONFLICT (email) DO NOTHING;
       `,
   ]);
