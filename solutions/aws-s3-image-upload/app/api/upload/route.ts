2f301c6891eaeca717266151335d4037a3aa3b98
Lee Robinson <me@leerob.io>
2023-11-16 12:44:05 -0600

    Update AWS S3 example. (#827)
    Replaces https://github.com/vercel/examples/pull/611.
    
    ---------
    
    Co-authored-by: bhris <contact@bhris.digital>

---
+++ solutions/aws-s3-image-upload/app/api/upload/route.ts
@@ -0,0 +1,28 @@
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post'
+import { S3Client } from '@aws-sdk/client-s3'
+import { v4 as uuidv4 } from 'uuid'
+
+export async function POST(request: Request) {
+  const { filename, contentType } = await request.json()
+
+  try {
+    const client = new S3Client({ region: process.env.AWS_REGION })
+    const { url, fields } = await createPresignedPost(client, {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: uuidv4(),
+      Conditions: [
+        ['content-length-range', 0, 10485760], // up to 10 MB
+        ['starts-with', '$Content-Type', contentType],
+      ],
+      Fields: {
+        acl: 'public-read',
+        'Content-Type': contentType,
+      },
+      Expires: 600, // Seconds before the presigned post expires. 3600 by default.
+    })
+
+    return Response.json({ url, fields })
+  } catch (error) {
+    return Response.json({ error: error.message })
+  }
+}
