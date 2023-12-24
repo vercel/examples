a94cccbc521c26936adebe46e5a35aab59e1c3cd
Luis Alvarez <luis@vercel.com>
2023-12-24 01:07:42 -0500

    Removed pages folder

--- solutions/cms-contentstack-commerce/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default App
