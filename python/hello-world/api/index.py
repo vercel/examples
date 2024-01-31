1e6c910fa708b99db9c01073cb1f1ce531ec1822
ksj0104 <154309849+ksj0104@users.noreply.github.com>
2024-01-31 12:01:35 +0900

    Update index.py

--- python/hello-world/api/index.py
+++ python/hello-world/api/index.py
@@ -6,5 +6,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write('Hello, sibal!'.encode('utf-8'))
         return
