c3c51204eb080eef00ca37159b565ef976d7d8f6
Andy Bitz <artzbitz@gmail.com>
2024-03-06 17:13:32 +0100

    [feature-flag-hypertune] Update `@vercel/flags` for Hypertune

--- edge-middleware/feature-flag-hypertune/app/.well-known/vercel/flags/route.ts
+++ edge-middleware/feature-flag-hypertune/app/.well-known/vercel/flags/route.ts
@@ -1,9 +1,10 @@
-import { NextResponse } from 'next/server'
-import { encryptApiData } from '@vercel/flags'
-import { vercelFlagDefinitions } from '../../../../generated/generated'
+import { type NextRequest, NextResponse } from 'next/server';
+import { verifyAccess, type ApiData } from '@vercel/flags';
+import { vercelFlagDefinitions } from '../../../../generated/generated';
 
-export async function GET() {
-  const apiData = { definitions: vercelFlagDefinitions }
-  const encryptedApiData = await encryptApiData(apiData)
-  return NextResponse.json(encryptedApiData)
+export async function GET(request: NextRequest) {
+  const access = await verifyAccess(request.headers.get('Authorization'));
+  if (!access) return NextResponse.json(null, { status: 401 });
+
+  return NextResponse.json<ApiData>({ definitions: vercelFlagDefinitions });
 }
