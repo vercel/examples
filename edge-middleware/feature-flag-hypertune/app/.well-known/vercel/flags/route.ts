594c3fc94da0c0b369003c340e94fc24c287b70f
Dominik Ferber <dominik.ferber@gmail.com>
2024-03-14 15:38:58 +0200

    upgrade hypertune example

--- edge-middleware/feature-flag-hypertune/app/.well-known/vercel/flags/route.ts
+++ edge-middleware/feature-flag-hypertune/app/.well-known/vercel/flags/route.ts
@@ -1,10 +1,14 @@
-import { type NextRequest, NextResponse } from 'next/server';
-import { verifyAccess, type ApiData } from '@vercel/flags';
-import { vercelFlagDefinitions } from '../../../../generated/generated';
+import { type NextRequest, NextResponse } from 'next/server'
+import { verifyAccess, type ApiData } from '@vercel/flags'
+import { getHypertuneData } from '@vercel/flags/providers/hypertune'
 
 export async function GET(request: NextRequest) {
-  const access = await verifyAccess(request.headers.get('Authorization'));
-  if (!access) return NextResponse.json(null, { status: 401 });
+  const access = await verifyAccess(request.headers.get('Authorization'))
+  if (!access) return NextResponse.json(null, { status: 401 })
 
-  return NextResponse.json<ApiData>({ definitions: vercelFlagDefinitions });
+  const data = await getHypertuneData({
+    token: process.env.NEXT_PUBLIC_HYPERTUNE_TOKEN!,
+  })
+
+  return NextResponse.json<ApiData>(data)
 }
