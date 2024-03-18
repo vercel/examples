a1b4ea9bf4c87ca9ce24a5f30345aa8530ef596d
ismaelrumzan <ismael.rumzan@gmail.com>
2024-03-18 14:13:23 -0600

    added emoji

---
+++ recipes/og-image-generation/app/emoji/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: 'Your page title',
+    description: 'Your page description',
+    url: 'https://vercel.com/',
+    type: 'website',
+    siteName: 'Vercel recipes',
+    images: [
+      {
+        url: `/api/emoji'
+        )}`,
+        alt: 'My Page Emoji',
+      },
+    ],
+  },
+}
+
+export default function Page() {
+  return <h1>Rendering an emoji in the image</h1>
+}
