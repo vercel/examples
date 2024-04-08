fa9097e2035ce35bfc4dda8697cafa383340e5a7
devjiwonchoi <devjiwonchoi@gmail.com>
2024-04-09 01:05:55 +0900

    chore: links to gitub

--- solutions/blog/app/components/footer.tsx
+++ solutions/blog/app/components/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/vercel/next.js"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/vercel/examples/tree/main/solutions/blog"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
