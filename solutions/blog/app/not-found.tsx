702f19f9635279416bb7e23459c3d2c7f254a6a6
jln-dk <jesper@jln.dk>
2024-04-09 09:55:24 +0400

    fix(solutions-blog): added not-found page

---
+++ solutions/blog/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        404 - Page Not Found
+      </h1>
+      <p className="mb-4">
+       The page you are looking for does not exist.
+      </p>
+    </section>
+  )
+}
