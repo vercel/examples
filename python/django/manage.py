7b95ee00e8afcad2d08b3cebea834150711ef452
Sean Massa <endangeredmassa@gmail.com>
2024-04-11 12:18:30 -0500

    python/django: remove use of "builds"

--- python/django/manage.py
+++ python/django/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
