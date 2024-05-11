8b75c1fcd7ad5f87932aeea7d40eb43002fa7df6
goncy <gonzalo.pozzo4@gmail.com>
2024-05-11 16:25:15 -0300

    Breadcrumbs example

---
+++ solutions/parallel-routes-navbar/app/(demo)/demo/[category]/[slug]/page.tsx
@@ -0,0 +1,59 @@
+export default function SlugPage() {
+  return (
+    <main>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porttitor
+      accumsan vestibulum. Mauris dictum hendrerit ex. Ut lacus libero, feugiat
+      sed turpis non, vehicula eleifend ex. Nulla in augue interdum, suscipit
+      diam eu, aliquam magna. Etiam convallis lacus mattis nulla aliquam, quis
+      tempor ante accumsan. Aenean non libero tempus mi eleifend pulvinar id ac
+      nulla. Nullam at nisi sed augue mattis varius ac quis purus. Vivamus
+      mollis arcu placerat venenatis commodo. Donec et dui iaculis, tristique
+      diam vel, commodo erat. In vel orci quis orci feugiat laoreet. Aliquam et
+      ante lacus. Vestibulum fermentum, ex sed elementum interdum, velit risus
+      elementum elit, nec malesuada enim dolor id ante. Nam justo velit,
+      imperdiet sed magna quis, lacinia faucibus lectus. In ut risus
+      ullamcorper, dignissim ligula ut, pharetra lacus. Etiam venenatis felis
+      sit amet elit pretium mollis. Sed condimentum nec dui ut efficitur. Duis
+      ornare lorem nec hendrerit auctor. Cras a porttitor turpis, tempor
+      facilisis mauris. Curabitur et luctus lacus, non lacinia elit. Aliquam
+      imperdiet rutrum leo eget fermentum. Nullam non massa fringilla tortor
+      eleifend posuere sed sit amet nisi. Mauris aliquet maximus fermentum.
+      Donec sit amet mauris et libero dapibus fringilla non sollicitudin nulla.
+      Curabitur non nunc et ex faucibus facilisis. Duis id arcu ac quam
+      tincidunt tincidunt in eget massa. Aenean ultrices eros lorem, a
+      scelerisque mi imperdiet eget. Maecenas justo nulla, ullamcorper id ligula
+      vitae, dictum tincidunt purus. Vestibulum pretium in neque eu fermentum.
+      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+      cubilia curae; Etiam elit libero, consequat ut eros a, vulputate rutrum
+      risus. Donec euismod fringilla augue vel suscipit. Suspendisse malesuada
+      lobortis molestie. Praesent efficitur luctus sodales. Morbi magna erat,
+      consequat ut eleifend sit amet, varius id nunc. Nunc vehicula diam eget
+      vestibulum efficitur. Vestibulum nec luctus urna. Aliquam laoreet odio id
+      dui tempus, non eleifend turpis lacinia. Maecenas lacinia, enim quis
+      hendrerit rutrum, velit est aliquam sem, eu accumsan erat erat quis enim.
+      Aliquam dolor dolor, porta et dolor sit amet, tempus hendrerit justo.
+      Donec dictum sed nisl ut viverra. Duis ornare magna malesuada leo aliquam
+      posuere. Aliquam in nunc ut ipsum pellentesque vehicula. Nulla ornare
+      scelerisque turpis, ut posuere felis. Morbi quis felis pretium urna
+      consectetur eleifend id eu metus. Proin at vulputate nibh, quis semper mi.
+      Nullam tincidunt maximus ex nec efficitur. Vestibulum id turpis vitae
+      massa venenatis lacinia. Nullam posuere massa eget leo consectetur
+      hendrerit. Donec sed augue eget ante dignissim pellentesque consectetur
+      quis enim. Maecenas ornare vitae magna ut aliquam. Nam enim elit, commodo
+      non vestibulum nec, convallis et nibh. Etiam facilisis, velit in venenatis
+      tincidunt, augue tellus pellentesque ligula, ac varius massa nunc ac
+      tellus. Curabitur porta eu nisl in placerat. Curabitur viverra et turpis
+      sagittis porttitor. Nam molestie, nisi in aliquam venenatis, diam nibh
+      pharetra tellus, ac tempus enim ante in erat. Fusce imperdiet urna sit
+      amet ante varius pellentesque. Aenean cursus maximus ligula, vitae
+      faucibus dui. Donec aliquam ipsum sed velit consequat, et egestas enim
+      vehicula. Curabitur auctor sapien at cursus venenatis. Nunc eget convallis
+      augue, condimentum malesuada lectus. Donec et scelerisque nulla. Morbi a
+      convallis velit. Suspendisse leo turpis, iaculis vestibulum pulvinar eget,
+      dapibus sit amet justo. Aliquam erat volutpat. Integer et lacus orci.
+      Integer non libero vestibulum, convallis erat ac, euismod mauris. Etiam
+      auctor nulla sed neque egestas condimentum. Duis sed metus malesuada,
+      cursus massa vitae, feugiat est.
+    </main>
+  )
+}
