07d1d58b83b14fab4f86e9fe9ec4b0281d4e2ee9
goncy <gonzalo.pozzo4@gmail.com>
2024-05-13 10:05:39 -0300

    Update texts

--- solutions/parallel-routes-navbar/app/(demo)/demo/[category]/[slug]/page.tsx
+++ solutions/parallel-routes-navbar/app/(demo)/demo/[category]/[slug]/page.tsx
@@ -7,18 +7,18 @@ const DATA = {
   nulla. Nullam at nisi sed augue mattis varius ac quis purus. Vivamus
   mollis arcu placerat venenatis commodo. Donec et dui iaculis, tristique
   diam vel, commodo erat. In vel orci quis orci feugiat laoreet.`,
-  'pumpkin-pie': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porttitor
-  accumsan vestibulum. Mauris dictum hendrerit ex. Ut lacus libero, feugiat
-  sed turpis non, vehicula eleifend ex. Nulla in augue interdum, suscipit
+  'pumpkin-pie': `Donec et dui iaculis, tristique diam vel, commodo erat.
+  In vel orci quis orci feugiat laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Morbi porttitor accumsan vestibulum. Mauris dictum hendrerit ex.
+  Ut lacus libero, feugiat sed turpis non, vehicula eleifend ex. Nulla in augue interdum, suscipit
   diam eu, aliquam magna. Etiam convallis lacus mattis nulla aliquam, quis
   tempor ante accumsan. Aenean non libero tempus mi eleifend pulvinar id ac
   nulla. Nullam at nisi sed augue mattis varius ac quis purus. Vivamus
-  mollis arcu placerat venenatis commodo. Donec et dui iaculis, tristique
-  diam vel, commodo erat. In vel orci quis orci feugiat laoreet.`,
-  'apple-pie': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porttitor
+  mollis arcu placerat venenatis commodo.`,
+  'apple-pie': `Nulla in augue interdum, suscipit
+  diam eu, aliquam magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porttitor
   accumsan vestibulum. Mauris dictum hendrerit ex. Ut lacus libero, feugiat
-  sed turpis non, vehicula eleifend ex. Nulla in augue interdum, suscipit
-  diam eu, aliquam magna. Etiam convallis lacus mattis nulla aliquam, quis
+  sed turpis non, vehicula eleifend ex. Etiam convallis lacus mattis nulla aliquam, quis
   tempor ante accumsan. Aenean non libero tempus mi eleifend pulvinar id ac
   nulla. Nullam at nisi sed augue mattis varius ac quis purus. Vivamus
   mollis arcu placerat venenatis commodo. Donec et dui iaculis, tristique
