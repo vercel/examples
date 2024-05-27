e58fbe80495345758ce10e763759c3589b9dae28
realmikesolo <aeryoff@gmail.com>
2024-05-27 19:38:10 +0200

    feat(drizzle): cleanup

--- storage/postgres-pgvector/drizzle.config.ts
+++ storage/postgres-pgvector/drizzle.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
-  // verbose: true, prints all statements that will be executed: https://orm.drizzle.team/kit-docs/config-reference#verbose
-  // strict: true, always ask for your confirmation to execute statements: https://orm.drizzle.team/kit-docs/config-reference#strict
+  // prints all statements that will be executed: https://orm.drizzle.team/kit-docs/config-reference#verbose
+  // verbose: true,
+  // always ask for your confirmation to execute statements: https://orm.drizzle.team/kit-docs/config-reference#strict
+  // strict: true,
 })
