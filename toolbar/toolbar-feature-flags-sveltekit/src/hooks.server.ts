7f74478588e4741b16d9d7eb3e7ea514ab3ddf01
Simon Holthausen <simon.holthausen@vercel.com>
2024-07-02 11:46:21 +0200

    mention readme

--- toolbar/toolbar-feature-flags-sveltekit/src/hooks.server.ts
+++ toolbar/toolbar-feature-flags-sveltekit/src/hooks.server.ts
@@ -4,4 +4,5 @@ import * as flags from '$lib/flags'
 
 // The `createHandle` function wires up the feature flags with the Vercel toolbar
 // and makes sure you can easily query the flags in your server load functions.
+// See the readme for more info on generating and retrieving the FLAGS_SECRET env var.
 export const handle = createHandle({ secret: FLAGS_SECRET, flags })
