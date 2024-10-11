5a41a5a3a21740bb2554e10449a6c828415e57a0
Fabio Benedetti <fabio.benedetti@vercel.com>
2024-10-11 10:26:35 +0200

    migrate kv-redis-nuxt

--- storage/kv-redis-nuxt/server/api/kv-demo.get.ts
+++ storage/kv-redis-nuxt/server/api/kv-demo.get.ts
@@ -1,8 +1,16 @@
+import Redis from 'ioredis'
+
 export default eventHandler(async () => {
-  const storage = useStorage('data')
-  let pageVisits = (await storage.getItem('pageVisits')) as number
+  if (!process.env.KV_URL) {
+    throw new Error(
+      'KV_URL is not set. It is either not set on your Vercel project or is not being pulled in the GitHub Actions workflow.'
+    )
+  }
+  const redis = new Redis(process.env.KV_URL)
+  const pageVisits = Number.parseInt((await redis.get('pageVisits')) || '0')
+
   const updatedPageVisits = pageVisits + 1
-  await storage.setItem('pageVisits', updatedPageVisits)
+  await redis.set('pageVisits', updatedPageVisits)
   return {
     pageVisits: updatedPageVisits,
   }
