60bc7bbe25ffaefdb203c018a7062ca318047514
jamsch <12927717+jamsch@users.noreply.github.com>
2024-11-13 13:15:13 +1300

    chore: update other examples using `req.geo`

--- edge-middleware/power-parity-pricing-strategies/middleware.ts
+++ edge-middleware/power-parity-pricing-strategies/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { geolocation } from '@vercel/functions'
+import { type NextRequest, NextResponse } from 'next/server'
 
 // Set pathname were middleware will be executed
 export const config = {
@@ -7,7 +8,7 @@ export const config = {
 
 export default function middleware(req: NextRequest) {
   // Get country
-  const country = req.geo.country?.toLowerCase() || 'us'
+  const country = geolocation(req).country?.toLowerCase() || 'us'
 
   // Update pathname
   req.nextUrl.pathname = `/edge/${country}`
