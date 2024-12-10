5adfc875055c3d54243525efed0a0e69c051a833
Hugo Charré <hgchrre@users.noreply.github.com>
2024-12-10 14:51:07 -0500

    Update Button component size across MFEs

--- solutions/microfrontends/packages/acme-design-system/src/button/button.tsx
+++ solutions/microfrontends/packages/acme-design-system/src/button/button.tsx
@@ -18,7 +18,7 @@ const Button: FC<ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>> = ({
   const rootClassName = cn(
     'relative inline-flex items-center justify-center cursor-pointer',
     'no-underline py-0 px-3.5 rounded-md border border-solid border-black',
-    'text-base font-medium outline-none select-none align-middle',
+    'text-4xl font-medium outline-none select-none align-middle',
     'whitespace-nowrap leading-10 shadow-md transition-colors',
     secondary ? 'bg-white text-black' : 'bg-black text-white',
     className
