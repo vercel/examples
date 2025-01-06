39d514e466822c5dab04043ee29e4851f2f276a3
Shusaku Uesugi <shu.chibicode@gmail.com>
2025-01-06 12:39:00 -0800

    Add vercel-tutor

---
+++ vercel-tutor/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Vercel Tutor",
+  description: "Get started with Vercel.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
