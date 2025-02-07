c58813d09bea2b5c782eb1a20269fc47efaeb24a
Fabio Benedetti <fabio.benedetti@vercel.com>
2025-02-07 16:53:20 +0100

    restor formatting

--- storage/postgres-drizzle/lib/drizzle.ts
+++ storage/postgres-drizzle/lib/drizzle.ts
@@ -1,34 +1,34 @@
 import {
-	pgTable,
-	serial,
-	text,
-	timestamp,
-	uniqueIndex,
-} from "drizzle-orm/pg-core";
-import { InferSelectModel, InferInsertModel } from "drizzle-orm";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core'
+import { InferSelectModel, InferInsertModel } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
 export const UsersTable = pgTable(
-	"profiles",
-	{
-		id: serial("id").primaryKey(),
-		name: text("name").notNull(),
-		email: text("email").notNull(),
-		image: text("image").notNull(),
-		createdAt: timestamp("createdAt").defaultNow().notNull(),
-	},
-	(users) => {
-		return {
-			uniqueIdx: uniqueIndex("unique_idx").on(users.email),
-		};
-	},
-);
+  'profiles',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    image: text('image').notNull(),
+    createdAt: timestamp('createdAt').defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+    }
+  }
+)
 
-export type User = InferSelectModel<typeof UsersTable>;
-export type NewUser = InferInsertModel<typeof UsersTable>;
+export type User = InferSelectModel<typeof UsersTable>
+export type NewUser = InferInsertModel<typeof UsersTable>
 
 // Connect to  Postgres
-export const db = drizzle(sql);
+export const db = drizzle(sql)
