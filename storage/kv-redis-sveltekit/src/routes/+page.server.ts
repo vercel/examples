3d4e18a5ac95c48a392749ea97395a4671ad9a28
Fabio Benedetti <fabio.benedetti@vercel.com>
2025-02-10 13:18:35 +0100

    move redis connection

--- storage/kv-redis-sveltekit/src/routes/+page.server.ts
+++ storage/kv-redis-sveltekit/src/routes/+page.server.ts
@@ -1,16 +1,17 @@
-import { REDIS_URL, KV_URL } from "$env/static/private";
-import { createClient } from "redis";
+import { REDIS_URL, KV_URL } from '$env/static/private'
+import { createClient } from 'redis'
+
+const kv = await createClient({
+  url: REDIS_URL ?? KV_URL,
+}).connect()
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
-	const kv = await createClient({
-		url: REDIS_URL ?? KV_URL,
-	}).connect();
-	const pageVisits = await kv.get("pageVisits");
-	await kv.set("pageVisits", Number.parseInt(pageVisits ?? "0", 10) + 1);
-	const updatedPageVisits = await kv.get("pageVisits");
+  const pageVisits = await kv.get('pageVisits')
+  await kv.set('pageVisits', Number.parseInt(pageVisits ?? '0', 10) + 1)
+  const updatedPageVisits = await kv.get('pageVisits')
 
-	return {
-		pageVisits: updatedPageVisits,
-	};
+  return {
+    pageVisits: updatedPageVisits,
+  }
 }
