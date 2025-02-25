6df9c9d4633102e64dc75b24b23b8720791b0649
Chris <7249920+chriswdmr@users.noreply.github.com>
2025-02-25 20:07:08 +0100

    Update middleware.ts

--- flags/experimentation-statsig/middleware.ts
+++ flags/experimentation-statsig/middleware.ts
@@ -1,13 +1,14 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { precompute } from 'flags/next';
-import { precomputeFlags, bannerFlags } from '@/flags';
+import { precomputeFlags } from '@/flags';
 import { getOrGenerateVisitorId } from './utils/generateVisitorId';
 
-export const config = { matcher: ['/', '/precomputed-pages-router-example'] };
+export const config = {
+  matcher: ['/']
+};
 
 export async function middleware(request: NextRequest) {
   const generatedVisitorId = await getOrGenerateVisitorId();
-  console.log('visitor', generatedVisitorId);
 
   if (request.nextUrl.pathname === '/') {
     const code = await precompute(precomputeFlags);
@@ -17,22 +18,12 @@ export async function middleware(request: NextRequest) {
       `/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
       request.url,
     );
+    
     return NextResponse.rewrite(nextUrl, {
       request,
       headers: generatedVisitorId.fresh
         ? { 'set-cookie': `visitor-id=${generatedVisitorId.value}` }
         : undefined,
     });
-  }
-
-  if (request.nextUrl.pathname === '/precomputed-pages-router-example') {
-    const code = await precompute(bannerFlags);
-
-    // rewrites the request to the variant for this flag combination
-    const nextUrl = new URL(
-      `/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
-      request.url,
-    );
-    return NextResponse.rewrite(nextUrl, { request });
   }
 }
