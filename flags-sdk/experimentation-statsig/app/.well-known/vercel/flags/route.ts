e2474e8d762cc636e457fc9de2b9dd96102720df
Dominik Ferber <dominik.ferber@gmail.com>
2025-02-26 19:10:07 +0200

    add Statsig API metadata for Flags Explorer

--- flags-sdk/experimentation-statsig/app/.well-known/vercel/flags/route.ts
+++ flags-sdk/experimentation-statsig/app/.well-known/vercel/flags/route.ts
@@ -1,15 +1,36 @@
-import { type ApiData, verifyAccess } from 'flags';
-import { getProviderData } from 'flags/next';
-import { NextResponse, type NextRequest } from 'next/server';
-import * as flags from '../../../../flags';
+import {
+  type ApiData,
+  verifyAccess,
+  mergeProviderData,
+  type ProviderData,
+} from 'flags'
+import { getProviderData } from 'flags/next'
+import { NextResponse, type NextRequest } from 'next/server'
+import * as flags from '../../../../flags'
+import { getProviderData as getStatsigProviderData } from '@flags-sdk/statsig'
 
-export const runtime = 'edge';
-export const dynamic = 'force-dynamic'; // defaults to auto
+export const runtime = 'edge'
+export const dynamic = 'force-dynamic' // defaults to auto
 
 export async function GET(request: NextRequest) {
-  const access = await verifyAccess(request.headers.get('Authorization'));
-  if (!access) return NextResponse.json(null, { status: 401 });
+  const access = await verifyAccess(request.headers.get('Authorization'))
+  if (!access) return NextResponse.json(null, { status: 401 })
 
-  const providerData = getProviderData(flags);
-  return NextResponse.json<ApiData>(providerData);
+  // Fetches additional metadata from the Statsig API for the Flags Explorer
+  let statsigData: ProviderData = { definitions: {}, hints: [] }
+  if (process.env.STATSIG_CONSOLE_API_KEY && process.env.STATSIG_PROJECT_ID) {
+    statsigData = await getStatsigProviderData({
+      statsigConsoleApiKey: process.env.STATSIG_CONSOLE_API_KEY,
+      projectId: process.env.STATSIG_PROJECT_ID,
+    })
+  }
+
+  const providerData = await mergeProviderData([
+    // Data declared from Flags in Code
+    getProviderData(flags),
+    // metadata from Statsig API
+    statsigData,
+  ])
+
+  return NextResponse.json<ApiData>(providerData)
 }
