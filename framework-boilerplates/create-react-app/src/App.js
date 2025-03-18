16e325de5d10d8ca81868ea77328669c3dd515ea
ehtishamqazi <82801236+ehtishamqazi@users.noreply.github.com>
2025-03-18 10:52:34 +0500

    Update App.js 2

--- framework-boilerplates/create-react-app/src/App.js
+++ framework-boilerplates/create-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jss</code> and save to reload.
         </p>
         <a
           className="App-link"
