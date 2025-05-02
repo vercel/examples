4b3a2ace78807f48a25bcebf0b283163110ce4ad
Aaron Morris <aaron@vercel.com>
2025-05-02 12:13:58 -0400

    (openfeature): update to flags v4

--- flags-sdk/openfeature/app/.well-known/vercel/flags/route.ts
+++ flags-sdk/openfeature/app/.well-known/vercel/flags/route.ts
@@ -1,16 +1,7 @@
-import { type ApiData, verifyAccess } from 'flags'
-import { getProviderData } from 'flags/next'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createFlagsDiscoveryEndpoint, getProviderData } from 'flags/next'
 import * as flags from '../../../../flags'
 
 export const runtime = 'edge'
 export const dynamic = 'force-dynamic' // defaults to auto
 
-export async function GET(request: NextRequest) {
-  const access = await verifyAccess(request.headers.get('Authorization'))
-  if (!access) return NextResponse.json(null, { status: 401 })
-
-  // Forward info from Flags in Code
-  const providerData = await getProviderData(flags)
-  return NextResponse.json<ApiData>(providerData)
-}
+export const GET = createFlagsDiscoveryEndpoint(() => getProviderData(flags))
