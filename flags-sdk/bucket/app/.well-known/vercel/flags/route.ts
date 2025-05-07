289bae93bc7a3e717a7ccf1bb28caf7bb7c8c08a
Aaron Morris <aaron@vercel.com>
2025-05-07 15:36:05 -0400

    Merge pull request #1 from AAorris/flags-sdk-bucket
    Update to flags@4

--- flags-sdk/bucket/app/.well-known/vercel/flags/route.ts
+++ flags-sdk/bucket/app/.well-known/vercel/flags/route.ts
@@ -1,21 +1,15 @@
-import { type ApiData, verifyAccess, mergeProviderData } from 'flags'
-import { getProviderData } from 'flags/next'
-import { NextResponse, type NextRequest } from 'next/server'
-import * as flags from '../../../../flags'
+import { getProviderData, createFlagsDiscoveryEndpoint } from 'flags/next'
 import { getProviderData as getBucketProviderData } from '@flags-sdk/bucket'
+import { mergeProviderData } from 'flags'
+import * as flags from '../../../../flags'
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
-export async function GET(request: NextRequest) {
-  const access = await verifyAccess(request.headers.get('Authorization'))
-  if (!access) return NextResponse.json(null, { status: 401 })
-
-  const providerData = await mergeProviderData([
+export const GET = createFlagsDiscoveryEndpoint(async (request) => {
+  return mergeProviderData([
     // Data declared from Flags in Code
     getProviderData(flags),
     // metadata from Bucket API using the default bucket adapter
     getBucketProviderData(),
   ])
-
-  return NextResponse.json<ApiData>(providerData)
-}
+})
