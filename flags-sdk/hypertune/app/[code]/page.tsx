6acca8386ac0a21fe377213f47a3d81fabfeb818
Aaron Morris <aaron@vercel.com>
2025-05-14 13:22:59 -0400

    (flags-sdk) Hypertune example for Flags SDK (#1131)
    ### Description
    
    Supercedes #1130 
    
    Introduces the Hypertune Adapter using [Flags
    SDK](https://flags-sdk.dev)

---
+++ flags-sdk/hypertune/app/[code]/page.tsx
@@ -0,0 +1,36 @@
+import { ImageGallery } from '@/components/image-gallery'
+import { ProductDetails } from '@/components/product-detail-page/product-details'
+import { ProductHeader } from '@/components/product-detail-page/product-header'
+import { AddToCart } from '@/components/shopping-cart/add-to-cart'
+import { ColorPicker } from '@/components/product-detail-page/color-picker'
+import { SizePicker } from '@/components/product-detail-page/size-picker'
+import { ProductDetailPageProvider } from '@/components/utils/product-detail-page-context'
+import { productFlags, showSummerBannerFlag } from '@/flags'
+import { Main } from '@/components/main'
+import { SummerSaleBanner } from '@/components/banners/summer-sale-banner'
+
+export default async function Page(props: {
+  params: Promise<{ code: string }>
+}) {
+  const { code } = await props.params
+  const showSummerBanner = await showSummerBannerFlag(code, productFlags)
+
+  return (
+    <ProductDetailPageProvider>
+      {showSummerBanner ? <SummerSaleBanner /> : null}
+      <Main>
+        <div className="lg:grid lg:auto-rows-min lg:grid-cols-12 lg:gap-x-8">
+          <ProductHeader />
+          <ImageGallery />
+
+          <div className="mt-8 lg:col-span-5">
+            <ColorPicker />
+            <SizePicker />
+            <AddToCart />
+            <ProductDetails />
+          </div>
+        </div>
+      </Main>
+    </ProductDetailPageProvider>
+  )
+}
