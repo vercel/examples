a282a65376780e63b2bf27f0696fddee33407178
Joshua Snyder <joshua@posthog.com>
2025-05-22 17:59:21 +0200

    fix: resolve app host correctly

--- flags-sdk/posthog/app/.well-known/vercel/flags/route.ts
+++ flags-sdk/posthog/app/.well-known/vercel/flags/route.ts
@@ -5,6 +5,24 @@ import * as flags from '../../../../flags'
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
+export const getAppHost = () => {
+  const host = process.env.NEXT_PUBLIC_POSTHOG_HOST
+
+  if (!host) {
+    throw new Error('NEXT_PUBLIC_POSTHOG_HOST is not set')
+  }
+
+  if (host.includes("us.i.posthog.com")) {
+    return "https://us.posthog.com"
+  }
+
+  if (host.includes("eu.i.posthog.com")) {
+    return "https://eu.posthog.com"
+  }
+
+  return host
+}
+
 export const GET = createFlagsDiscoveryEndpoint(async (request) => {
 
   return mergeProviderData([
@@ -13,7 +31,7 @@ export const GET = createFlagsDiscoveryEndpoint(async (request) => {
     // metadata from PostHog API using the default posthog adapter
     getPostHogProviderData({
       personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY,
-      host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      host: getAppHost(),
       projectId: process.env.POSTHOG_PROJECT_ID,
     }),
   ])
