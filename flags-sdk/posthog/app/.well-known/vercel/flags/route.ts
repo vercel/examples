afe404cd865cb88e75bdc9f68971b8e2e1f64c53
Joshua Snyder <joshua@posthog.com>
2025-05-23 15:53:14 +0200

    fix: remove not required request arg

--- flags-sdk/posthog/app/.well-known/vercel/flags/route.ts
+++ flags-sdk/posthog/app/.well-known/vercel/flags/route.ts
@@ -6,7 +6,7 @@ import * as flags from '../../../../flags'
 export const dynamic = 'force-dynamic' // defaults to auto
 
 export const GET = createFlagsDiscoveryEndpoint(
-  async (request) => {
+  async () => {
     return mergeProviderData([
       // Data declared from Flags in Code
       getProviderData(flags),
