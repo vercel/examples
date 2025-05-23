5dea773d2a55f123ed771e56e9d63c5f445c4805
Joshua Snyder <joshua@posthog.com>
2025-05-23 15:52:46 +0200

    chore: remove stale app host logic

--- flags-sdk/posthog/app/.well-known/vercel/flags/route.ts
+++ flags-sdk/posthog/app/.well-known/vercel/flags/route.ts
@@ -5,36 +5,19 @@ import * as flags from '../../../../flags'
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
-export const getAppHost = () => {
-  const host = process.env.NEXT_PUBLIC_POSTHOG_HOST
-
-  if (!host) {
-    throw new Error('NEXT_PUBLIC_POSTHOG_HOST is not set')
+export const GET = createFlagsDiscoveryEndpoint(
+  async (request) => {
+    return mergeProviderData([
+      // Data declared from Flags in Code
+      getProviderData(flags),
+      // metadata from PostHog API using the default posthog adapter
+      getPostHogProviderData({
+        personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY,
+        projectId: process.env.POSTHOG_PROJECT_ID,
+      }),
+    ])
+  },
+  {
+    secret: process.env.FLAGS_SECRET,
   }
-
-  if (host.includes("us.i.posthog.com")) {
-    return "https://us.posthog.com"
-  }
-
-  if (host.includes("eu.i.posthog.com")) {
-    return "https://eu.posthog.com"
-  }
-
-  return host
-}
-
-export const GET = createFlagsDiscoveryEndpoint(async (request) => {
-
-  return mergeProviderData([
-    // Data declared from Flags in Code
-    getProviderData(flags),
-    // metadata from PostHog API using the default posthog adapter
-    getPostHogProviderData({
-      personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY,
-      host: getAppHost(),
-      projectId: process.env.POSTHOG_PROJECT_ID,
-    }),
-  ])
-}, {
-  secret: process.env.FLAGS_SECRET
-})
+)
