7ab6a8a6f0b040ac1d2492ea2e84ffee0a5e7937
Joe Zeng <joezeng6@gmail.com>
2025-06-11 15:00:48 -0700

    migrate from framer-motion to motion package

--- flags-sdk/growthbook/components/shopping-cart/shopping-cart-item.tsx
+++ flags-sdk/growthbook/components/shopping-cart/shopping-cart-item.tsx
@@ -2,7 +2,7 @@
 import type { CartItem } from '@/components/utils/cart-types'
 import { ShoppingBagIcon } from '@heroicons/react/24/outline'
 import { colorToImage } from '@/components/utils/images'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import Image from 'next/image'
 import { ShoppingCartRemoveButton } from './shopping-cart-remove-button'
 import Link from 'next/link'
