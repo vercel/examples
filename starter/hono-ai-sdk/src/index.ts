7a19277d2a8c5b03c48053d4dbd6f95533d30ca8
Anthony Shew <anthony.shew@vercel.com>
2025-07-31 20:24:46 -0600

    Improve initial response for Hono AI SDK example. (#1171)
    ### Description
    
    <!--
    ✍️ Write a short summary of your work. Screenshots and videos are
    welcome!
    -->
    
    ### Demo URL
    
    <!--
    Provide a URL to a live deployment where we can test your PR. If a demo
    isn't possible feel free to omit this section.
    -->
    
    ### Type of Change
    
    - [ ] New Example
    - [ ] Example updates (Bug fixes, new features, etc.)
    - [ ] Other (changes to the codebase, but not to examples)
    
    ### New Example Checklist
    
    - [ ] 🛫 `npm run new-example` was used to create the example
    - [ ] 📚 The template wasn't used but I carefuly read the [Adding a new
    example](https://github.com/vercel/examples#adding-a-new-example) steps
    and implemented them in the example
    - [ ] 📱 Is it responsive? Are mobile and tablets considered?

--- starter/hono-ai-sdk/src/index.ts
+++ starter/hono-ai-sdk/src/index.ts
@@ -4,13 +4,17 @@ import { stream } from 'hono/streaming'
 
 const app = new Hono()
 
+const prompt = 'Why is the sky blue?'
+
 app.get('/', async (c) => {
   const result = streamText({
     model: 'xai/grok-3',
-    prompt: 'Why is the sky blue?',
+    prompt,
   })
 
   return stream(c, async (stream) => {
+    const prependStream = `Prompt: ${prompt}\n\n`
+    await stream.write(prependStream)
     for await (const textPart of result.textStream) {
       await stream.write(textPart)
     }
