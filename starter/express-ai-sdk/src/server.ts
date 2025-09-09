3c05b24acf53cf56b3a8710416c5d00c9f1e13a4
Anthony Shew <anthonyshew@gmail.com>
2025-09-09 13:13:50 -0600

    Express + AI SDK

---
+++ starter/express-ai-sdk/src/server.ts
@@ -0,0 +1,48 @@
+import { openai } from '@ai-sdk/openai'
+import {
+  createUIMessageStream,
+  pipeUIMessageStreamToResponse,
+  streamText,
+} from 'ai'
+import 'dotenv/config'
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.post('/', async (req: Request, res: Response) => {
+  const result = streamText({
+    model: openai('gpt-4o'),
+    prompt: 'Invent a new holiday and describe its traditions.',
+  })
+
+  result.pipeUIMessageStreamToResponse(res)
+})
+
+app.post('/custom-data-parts', async (req: Request, res: Response) => {
+  pipeUIMessageStreamToResponse({
+    response: res,
+    stream: createUIMessageStream({
+      execute: async ({ writer }) => {
+        writer.write({ type: 'start' })
+
+        writer.write({
+          type: 'data-custom',
+          data: {
+            custom: 'Hello, world!',
+          },
+        })
+
+        const result = streamText({
+          model: openai('gpt-4o'),
+          prompt: 'Invent a new holiday and describe its traditions.',
+        })
+
+        writer.merge(result.toUIMessageStream({ sendStart: false }))
+      },
+    }),
+  })
+})
+
+app.listen(8080, () => {
+  console.log(`Example app listening on port ${8080}`)
+})
