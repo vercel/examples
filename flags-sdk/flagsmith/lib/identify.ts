4af739281081011a318210ec0ba5b1738eaf475f
Dominik Ferber <dominik.ferber@gmail.com>
2025-10-16 16:55:39 +0300

    [flagsmith] identify with object

--- flags-sdk/flagsmith/lib/identify.ts
+++ flags-sdk/flagsmith/lib/identify.ts
@@ -1,15 +1,11 @@
 import type { Identify } from 'flags'
 import { dedupe } from 'flags/next'
 import { getStableId } from './get-stable-id'
-import type { IIdentity } from 'flagsmith/types'
+import type { EntitiesType } from '@flags-sdk/flagsmith'
 
 export const identify = dedupe(async () => {
   const stableId = await getStableId()
-
-  // You would call traits.put() here to persist traits you identified,
-  // but Flagsmith expects you to return a singular string as the identity.
-  //
-  // See https://docs.flagsmith.com/basic-features/managing-identities
-  console.log('stableId', stableId)
-  return stableId.value
-}) satisfies Identify<IIdentity>
+  return {
+    targetingKey: stableId.value,
+  }
+}) satisfies Identify<EntitiesType>
