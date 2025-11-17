d47cf7b615c6f2f8bb12dc1250aeceb2b2bc3ca3
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-17 20:28:25 +0530

    Upgrade postgres-prisma example to prisma 7

--- storage/postgres-prisma/lib/prisma.ts
+++ storage/postgres-prisma/lib/prisma.ts
@@ -1,10 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/generated/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+const adapter = new PrismaPg({
+  connectionString: process.env.POSTGRES_PRISMA_URL!,
+})
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    adapter,
+  })
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
