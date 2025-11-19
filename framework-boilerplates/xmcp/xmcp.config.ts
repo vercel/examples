f303c19c963e1aa1de12e6aa619fa89f2b625430
Valentina Bearzotti <64658886+valebearzotti@users.noreply.github.com>
2025-11-19 15:26:35 -0300

    update xmcp to v0.5.0 (#1221)
    ### Description
    xmcp now features prompts on its latest release, would be great to bump
    to v0.2 so it initializes with it by default
    
    ### Type of Change
    
    - [ ] New Example
    - [X] Example updates (Bug fixes, new features, etc.)
    - [ ] Other (changes to the codebase, but not to examples)

--- framework-boilerplates/xmcp/xmcp.config.ts
+++ framework-boilerplates/xmcp/xmcp.config.ts
@@ -1,7 +1,12 @@
-import { type XmcpConfig } from 'xmcp'
+import { type XmcpConfig } from "xmcp";
 
 const config: XmcpConfig = {
   http: true,
-}
+  paths: {
+    tools: "./src/tools",
+    prompts: "./src/prompts",
+    resources: "./src/resources",
+  }
+};
 
-export default config
+export default config;
