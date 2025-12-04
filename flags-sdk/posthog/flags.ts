e942e953cded65174bc1b144af5cd997c03a06a8
Dominik Ferber <dominik.ferber@gmail.com>
2025-12-04 10:30:09 +0200

    fix posthog

--- flags-sdk/posthog/flags.ts
+++ flags-sdk/posthog/flags.ts
@@ -1,6 +1,7 @@
 import { postHogAdapter, type PostHogEntities } from '@flags-sdk/posthog'
 import { flag } from 'flags/next'
 import { identify } from './lib/identify'
+import type { Adapter } from 'flags'
 
 export const showSummerBannerFlag = flag<boolean, PostHogEntities>({
   key: 'summer_sale',
@@ -18,7 +19,11 @@ export const showFreeDeliveryBannerFlag = flag<boolean, PostHogEntities>({
 
 export const proceedToCheckoutColorFlag = flag<string, PostHogEntities>({
   key: 'proceed_to_checkout_color',
-  adapter: postHogAdapter.featureFlagValue(),
+  // This is a limitation in the PostHog adapter which does not take generic type
+  adapter: postHogAdapter.featureFlagValue() as Adapter<
+    string,
+    PostHogEntities
+  >,
   defaultValue: 'blue',
   options: ['blue', 'green', 'red'],
   identify,
