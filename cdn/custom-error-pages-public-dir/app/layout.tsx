65817f1256f2ab267cfe2ed5def59a7fcd52c12f
Chandan Rao <chandan.rao@vercel.com>
2026-01-23 17:02:31 -0800

    [cdn] fixing minor page rendering issues

--- cdn/custom-error-pages-public-dir/app/layout.tsx
+++ cdn/custom-error-pages-public-dir/app/layout.tsx
@@ -5,11 +5,13 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  adjustFontFallback: true,
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  adjustFontFallback: true,
 });
 
 export const metadata: Metadata = {
