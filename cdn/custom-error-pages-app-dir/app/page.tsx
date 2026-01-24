32e5f777ed316cf77b5133a65b26e3f58b6dc178
Chandan Rao <chandan.rao@vercel.com>
2026-01-23 16:38:15 -0800

    [cdn] updating pages based on the design recommendations (#1391)
    ### Description
    Updating the examples to adhere to the design guidelines
    
    ### Type of Change
    
    - [ ] New Example
    - [x] Example updates (Bug fixes, new features, etc.)
    - [ ] Other (changes to the codebase, but not to examples)
    
    ### New Example Checklist
    
    - [ ] 🛫 `npm run new-example` was used to create the example
    - [ ] 📚 The template wasn't used but I carefuly read the [Adding a new
    example](https://github.com/vercel/examples#adding-a-new-example) steps
    and implemented them in the example
    - [ ] 📱 Is it responsive? Are mobile and tablets considered?

--- cdn/custom-error-pages-app-dir/app/page.tsx
+++ cdn/custom-error-pages-app-dir/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-16 px-8 bg-white dark:bg-black sm:items-start sm:px-16 sm:py-24">
+      <main className="flex w-full max-w-3xl flex-col items-center gap-8 py-12 px-8 bg-white dark:bg-black sm:items-start sm:px-16">
         <Image
           className="dark:invert"
           src="/vercel.svg"
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-lg text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-lg text-3xl font-semibold leading-10 tracking-tight text-black text-balance dark:text-zinc-50">
             Custom Error Pages with App Router
           </h1>
           <p className="max-w-lg text-base leading-7 text-zinc-600 dark:text-zinc-400">
@@ -67,13 +67,13 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row sm:flex-wrap">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:w-auto"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-zinc-950 dark:hover:bg-[#ccc] dark:focus-visible:ring-zinc-300 sm:w-auto"
             href="/slow-page"
           >
             Trigger Timeout
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:w-auto"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-zinc-950 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] dark:focus-visible:ring-zinc-300 sm:w-auto"
             href="https://github.com/vercel/examples/tree/main/cdn/custom-error-pages-app-dir"
             target="_blank"
             rel="noopener noreferrer"
@@ -81,7 +81,7 @@ export default function Home() {
             View Source
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:w-auto"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-zinc-950 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] dark:focus-visible:ring-zinc-300 sm:w-auto"
             href="https://vercel.com/docs/custom-error-pages"
             target="_blank"
             rel="noopener noreferrer"
