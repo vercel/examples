ef4d9f7162f3cc20c46f48285017b2ff2ad970f6
dnwpark <dnwpark@protonmail.com>
2026-06-24 13:05:11 -0700

    Binding trailing / removed.

--- python/nextjs-fastapi-bindings/frontend/app/api/backend/[[...path]]/route.js
+++ python/nextjs-fastapi-bindings/frontend/app/api/backend/[[...path]]/route.js
@@ -1,7 +1,7 @@
 export async function GET(request, { params }) {
   const { path } = await params;
-  const search = request.nextUrl.search;
-  const url = `${process.env.BACKEND_URL}${path ? path.join("/") : ""}${search}`;
+  const url = new URL(path ? path.join("/") : "", process.env.BACKEND_URL);
+  url.search = request.nextUrl.search
   const res = await fetch(url, { cache: "no-store" });
   const data = await res.json();
   return Response.json(data, { status: res.status });
