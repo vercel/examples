3593cc43a48cecd830b8be04c712f5d840e74583
Florentin Eckl <ecklf@icloud.com>
2026-08-10 10:56:17 +0200

    chore: address feedback

--- edge-middleware/redirects-upstash/scripts/upstash.js
+++ edge-middleware/redirects-upstash/scripts/upstash.js
@@ -61,7 +61,7 @@ async function setupUpstash() {
 
   await writeFile(filePath, content)
 
-  if (process.env.POPULATE_REDIS === 'false') {
+  if (process.env.POPULATE_REDIS !== 'true') {
     console.log('Skipping redis population of redirects')
     return
   }
