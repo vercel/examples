9e9a86e027a0b98a386ef848dffa7f64046b469c
test4 tester <129642211+Rohitp6828@users.noreply.github.com>
2026-08-29 21:16:57 +0530

    Update App.js

--- framework-boilerplates/create-react-app/src/App.js
+++ framework-boilerplates/create-react-app/src/App.js
@@ -6,12 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        //<p>
+        //  Edit <code>src/App.js</code> and save to reload.
+        //</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://civil-construction-procurement.codata-io.app"
           target="_blank"
           rel="noopener noreferrer"
         >
